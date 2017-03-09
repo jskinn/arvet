283f846264c23ff4bba4e65c315124c23e708a07
= <=>
2017-03-09 11:14:26 +1000

    Update geometry tests

--- util/tests/test_geometry.py
+++ util/tests/test_geometry.py
@@ -14,12 +14,19 @@ class TestGeometry(unittest.TestCase):
         self.assertEquals(result[2], 3)
 
     def test_dict_vector_to_np_array_causes_exception_for_missing_keys(self):
-        got_error = False
-        try:
+        with self.assertRaises(KeyError):
             g.dict_vector_to_np_array({'x': 1, 'Y': 2, 'Z': 3})
-        except KeyError:
-            got_error = True
-        self.assertTrue(got_error)
+        with self.assertRaises(KeyError):
+            g.dict_vector_to_np_array({'Y': 2, 'Z': 3})
+        with self.assertRaises(KeyError):
+            g.dict_vector_to_np_array({'X': 1, 'y': 2, 'Z': 3})
+        with self.assertRaises(KeyError):
+            g.dict_vector_to_np_array({'X': 1, 'Z': 3})
+        with self.assertRaises(KeyError):
+            g.dict_vector_to_np_array({'Z': 1, 'Y': 2, 'z': 3})
+        with self.assertRaises(KeyError):
+            g.dict_vector_to_np_array({'X': 1, 'Y': 2})
+
 
     def test_dict_quaternion_to_np_array(self):
         result = g.dict_quaternion_to_np_array({'X': 1, 'Y': 2, 'Z': 3, 'W': 4})
@@ -29,6 +36,24 @@ class TestGeometry(unittest.TestCase):
         self.assertEquals(result[1], 2)
         self.assertEquals(result[2], 3)
         self.assertEquals(result[3], 4)
+
+    def test_dict_quaternion_to_np_array_causes_exception_for_missing_keys(self):
+        with self.assertRaises(KeyError):
+            g.dict_quaternion_to_np_array({'x': 1, 'Y': 2, 'Z': 3, 'W': 4})
+        with self.assertRaises(KeyError):
+            g.dict_quaternion_to_np_array({'Y': 2, 'Z': 3, 'W': 4})
+        with self.assertRaises(KeyError):
+            g.dict_quaternion_to_np_array({'X': 1, 'y': 2, 'Z': 3, 'W': 4})
+        with self.assertRaises(KeyError):
+            g.dict_quaternion_to_np_array({'X': 1, 'Z': 3, 'W': 4})
+        with self.assertRaises(KeyError):
+            g.dict_quaternion_to_np_array({'X': 1, 'Y': 2, 'z': 3, 'W': 4})
+        with self.assertRaises(KeyError):
+            g.dict_quaternion_to_np_array({'X': 1, 'Y': 2, 'W': 4})
+        with self.assertRaises(KeyError):
+            g.dict_quaternion_to_np_array({'X': 1, 'Y': 2, 'Z': 3, 'w': 4})
+        with self.assertRaises(KeyError):
+            g.dict_quaternion_to_np_array({'X': 1, 'Y': 2, 'Z': 3})
 
     def test_numpy_vector_to_dict(self):
         result = g.numpy_vector_to_dict(np.array([1,2,3]))
@@ -47,5 +72,4 @@ class TestGeometry(unittest.TestCase):
             x = a * 1.63436326 * np.sin(a)
             y = x * 8.46546780 * np.sin(x)
             z = y * 4.27345834 * np.sin(y)
-            print("{0}, {1}, {2}".format(x,y,z))
             self.assertEquals(g.square_length(np.array([x, y, z])), x*x + y*y + z*z)
