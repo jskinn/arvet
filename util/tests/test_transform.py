db0a6980ebb8f600a2de6a534428900a53446a81
= <=>
2017-03-16 22:11:18 +1000

    Add a transform class for handling different expressions of pose in 3D

---
+++ util/tests/test_transform.py
@@ -0,0 +1,155 @@
+import unittest
+import numpy as np
+import util.transform as trans
+
+
+def _make_quat(axis, theta):
+    ax = np.asarray(axis)
+    q = np.zeros(4)
+    q[0] = np.cos(theta / 2)
+    q[1:] = np.sin(theta / 2) * (ax / np.linalg.norm(ax))
+    return q
+
+
+class TestTransform(unittest.TestCase):
+
+    def test_location_basic(self):
+        tf = trans.Transform(location=(1, 2, 3))
+        self.assert_array((1, 2, 3), tf.location)
+
+    def test_location_default(self):
+        tf = trans.Transform()
+        self.assert_array(np.zeros(3), tf.location)
+
+    def test_location_from_homogeneous(self):
+        hom = np.array([[0.80473785, -0.31061722, 0.50587936, 1],
+                        [0.50587936, 0.80473785, -0.31061722, 2],
+                        [-0.31061722, 0.50587936, 0.80473785, 3],
+                        [0, 0, 0, 1]])
+        tf = trans.Transform(hom)
+        self.assert_array((1, 2, 3), tf.location)
+
+    def test_rotation_basic(self):
+        # The rotation here is for 45 degrees around axis 1,2,3
+        tf = trans.Transform(location=(1, 2, 3), rotation=(0.92387953, 0.10227645, 0.2045529, 0.30682935), w_first=True)
+        self.assert_array(tf.rotation_quat(w_first=True),
+                          np.array([0.92387953, 0.10227645, 0.2045529, 0.30682935]))
+        self.assert_array(tf.rotation_quat(w_first=False),
+                          np.array([0.10227645, 0.2045529, 0.30682935, 0.92387953]))
+
+    def test_rotation_default(self):
+        tf = trans.Transform()
+        self.assert_array(tf.rotation_quat(True), (1, 0, 0, 0))
+
+    def test_rotation_from_homogeneous(self):
+        hom = np.array([[0.80473785, -0.31061722, 0.50587936, 1],
+                        [0.50587936, 0.80473785, -0.31061722, 2],
+                        [-0.31061722, 0.50587936, 0.80473785, 3],
+                        [0, 0, 0, 1]])
+        tf = trans.Transform(hom)
+        self.assert_close(tf.rotation_quat(True), (0.92387953, 0.22094238, 0.22094238, 0.22094238))
+
+    def test_euler_each_axis(self):
+        # Pitch
+        qrot = _make_quat((0, 0, 1), np.pi / 6)
+        tf = trans.Transform(rotation=qrot, w_first=True)
+        self.assert_array(tf.euler, np.array([np.pi / 6, 0, 0]))
+        # Yaw
+        qrot = _make_quat((0, 1, 0), np.pi / 6)
+        tf = trans.Transform(rotation=qrot, w_first=True)
+        self.assert_array(tf.euler, np.array([0, np.pi / 6, 0]))
+        # Roll
+        qrot = _make_quat((1, 0, 0), np.pi / 6)
+        tf = trans.Transform(rotation=qrot, w_first=True)
+        self.assert_array(tf.euler, np.array([0, 0, np.pi / 6]))
+
+    def test_find_relative_point_moves_origin(self):
+        point = (11, 12, 13)
+        tf = trans.Transform(location=(10, 9, 8))
+        point_rel = tf.find_relative(point)
+        self.assert_array(point_rel, (1, 3, 5))
+
+    def test_find_relative_pose_moves_origin(self):
+        pose = trans.Transform(location=(11, 12, 13))
+        tf = trans.Transform(location=(10, 9, 8))
+        pose_rel = tf.find_relative(pose)
+        self.assert_array(pose_rel.location, (1, 3, 5))
+
+    def test_find_relative_point_changes_location_coordinates(self):
+        point = (11, 12, 13)
+        tf = trans.Transform(location=(10, 9, 8), rotation=_make_quat((0, 0, 1), np.pi / 2), w_first=True)
+        point_rel = tf.find_relative(point)
+        self.assert_close(point_rel, (3, -1, 5))
+
+    def test_find_relative_pose_changes_location_coordinates(self):
+        pose = trans.Transform(location=(11, 12, 13))
+        tf = trans.Transform(location=(10, 9, 8), rotation=_make_quat((0, 0, 1), np.pi / 2), w_first=True)
+        pose_rel = tf.find_relative(pose)
+        self.assert_close(pose_rel.location, (3, -1, 5))
+
+    def test_find_relative_pose_changes_orientation(self):
+        pose = trans.Transform(location=(11, 12, 13), rotation=_make_quat((1, 0, 0), np.pi / 4), w_first=True)
+        tf = trans.Transform(location=(10, 9, 8), rotation=_make_quat((0, 0, 1), np.pi / 2), w_first=True)
+        pose_rel = tf.find_relative(pose)
+        self.assert_close(pose_rel.euler, (-np.pi / 2, -np.pi / 4, 0))
+
+    def test_find_independent_point_moves_origin(self):
+        point = (1, 3, 5)
+        tf = trans.Transform(location=(10, 9, 8))
+        point_rel = tf.find_independent(point)
+        self.assert_array(point_rel, (11, 12, 13))
+
+    def test_find_independent_pose_moves_origin(self):
+        pose = trans.Transform(location=(1, 3, 5))
+        tf = trans.Transform(location=(10, 9, 8))
+        pose_rel = tf.find_independent(pose)
+        self.assert_array(pose_rel.location, (11, 12, 13))
+
+    def test_find_relative_undoes_point(self):
+        loc = (-13, 27, -127)
+        qrot = _make_quat((-1, 0.1, -0.37), 7 * np.pi / 26)
+        tf = trans.Transform(location=loc, rotation=qrot, w_first=True)
+
+        point = (1, 2, 3)
+        point_rel = tf.find_relative(point)
+        point_prime = tf.find_independent(point_rel)
+        self.assert_close(point_prime, point)
+
+    def test_find_relative_undoes_pose(self):
+        loc = (-13, 27, -127)
+        qrot = _make_quat(np.array((-1, 0.1, -0.37)), 7 * np.pi / 26)
+        tf = trans.Transform(location=loc, rotation=qrot, w_first=True)
+
+        pose = trans.Transform(location=(10, 100, -5), rotation=_make_quat((0, 1, 0), np.pi / 4), w_first=True)
+        pose_rel = tf.find_relative(pose)
+        pose_prime = tf.find_independent(pose_rel)
+
+        self.assert_close(pose_prime.location, pose.location)
+        self.assert_close(pose_prime.rotation_quat(w_first=True), pose.rotation_quat(w_first=True))
+
+    def test_relative_pose_contains_relative_point(self):
+        loc = (-13, 27, -127)
+        qrot = _make_quat(np.array((-1, 0.1, -0.37)), 7 * np.pi / 26)
+        tf = trans.Transform(location=loc, rotation=qrot, w_first=True)
+
+        point = np.array([41, -153, 16])
+        pose = trans.Transform(location=point, rotation=_make_quat((0, 1, 0), np.pi / 4), w_first=True)
+        pose_rel = tf.find_relative(pose)
+        point_rel = pose_rel.location
+        point_prime = tf.find_independent(point_rel)
+
+        self.assert_close(point, point_prime)
+
+    def assert_array(self, arr1, arr2, msg=None):
+        a1 = np.asarray(arr1)
+        a2 = np.asarray(arr2)
+        if msg is None:
+            msg = "{0} is not equal to {1}".format(str(a1), str(a2))
+        self.assertTrue(np.array_equal(a1, a2), msg)
+
+    def assert_close(self, arr1, arr2, msg=None):
+        a1 = np.asarray(arr1)
+        a2 = np.asarray(arr2)
+        if msg is None:
+            msg = "{0} is not close to {1}".format(str(a1), str(a2))
+        self.assertTrue(np.all(np.isclose(a1, a2)), msg)
---
+++ util/transform.py
@@ -0,0 +1,178 @@
+import numpy as np
+import transforms3d as tf
+
+
+######
+#TODO: We need to handle transforming between coordinate frames where the axies don't point in the same direction
+# for instance, Unreal's coordinates have X as forward, Y as up, and Z as right
+# but an algorithm might have Z as up and Y as left, or Z as forward and X as right
+# I need to establish what coordinate frame I'm using, and write code to convert everyone else's to mine.
+#####
+
+class Transform:
+    """
+    A class for handling everything to do with specifying a location and orientation in 3D space.
+    Different systems have different conventions, you need to keep track of:
+    - Handedness (left or right)
+    - Which axis points in which direction
+    - Order of rotations for euler angles
+
+    I'm going to establish conventions here, and then for each system, convert to and from them.
+    THESES ARE THE RULES:
+    - X is forward, Y is up, Z is right
+    - This is a right-handed coordinate system
+    - Euler angles are applied Z, Y, X for roll, pitch, yaw, matching tait-bryan approach
+    YOU MUST CONVERT TO THIS COORDINATE SYSTEM
+
+    This class can handle quaternions with W either as the first or last element.
+    """
+
+    __slots__ = ['_x', '_y', '_z', '_qx', '_qy', '_qz', '_qw']
+
+    def __init__(self, location=None, rotation=None, w_first=False):
+        if isinstance(location, np.ndarray) and location.shape == (4, 4):
+            # first parameter is a homogenous transformation matrix, turn it back into
+            # location and quaternion
+            trans, rot, zooms, shears = tf.affines.decompose44(location)
+            self._x, self._y, self._z = trans
+            self._qw, self._qx, self._qy, self._qz = tf.quaternions.mat2quat(rot)
+        else:
+            # Location
+            if location is not None and len(location) >= 3:
+                self._x, self._y, self._z = location
+            else:
+                self._x = self._y = self._z = 0
+            # rotation
+            if rotation is not None and len(rotation) == 3:
+                # Rotation is euler angles, convert to quaternion
+                # I'm using the tait-bryan order of operations, for consistency with Unreal
+                self._qw, self._qx, self._qy, self._qz = tf.taitbryan.euler2quat(rotation[2], rotation[1], rotation[0])
+            elif rotation is not None and len(rotation) >= 4:
+                if w_first:
+                    self._qw, self._qx, self._qy, self._qz = rotation
+                else:
+                    self._qx, self._qy, self._qz, self._qw = rotation
+            else:
+                self._qw = 1
+                self._qx = self._qy = self._qz = 0
+
+    @property
+    def location(self):
+        """
+        Get the location represented by this pose.
+        :return: A numpy
+        """
+        return np.array([self._x, self._y, self._z])
+
+    def rotation_quat(self, w_first=False):
+        """
+        Get the unit quaternion representing the orientation of this pose.
+        :param w_first: Is the scalar parameter the first or last element in the vector.
+        :return: A 4-element numpy array that is the unit quaternion orientation
+        """
+        if w_first:
+            return np.array([self._qw, self._qx, self._qy, self._qz])
+        return np.array([self._qx, self._qy, self._qz, self._qw])
+
+    @property
+    def euler(self):
+        """
+        Get the Tait-Bryan angles for the rotation of this transform.
+        Expressed as a numpy vector (pitch, yaw, roll),
+        :return: A numpy array containing the euler angles
+        """
+        return tf.taitbryan.quat2euler((self._qw, self._qx, self._qy, self._qz))
+
+    @property
+    def transform_matrix(self):
+        """
+        Get the homogenous transformation matrix for this pose
+        :return:
+        """
+        return tf.affines.compose(self.location,
+                                  tf.quaternions.quat2mat(self.rotation_quat(w_first=True)),
+                                  np.ones(3))
+
+    @property
+    def forward(self):
+        """
+        Get the forward vector of the transform.
+        That is, the positive X direction in the local coordinate frame,
+        transformed to the outer coordinate frame.
+        :return: The direction the pose is "facing"
+        """
+        return self.find_independent((1, 0, 0))
+
+    @property
+    def back(self):
+        """
+        Get the back vector of the transform.
+        That is, the negative X direction of the local coordinate frame,
+        transformed to the outer coordinate frame.
+        :return: The direction backwards from the pose
+        """
+        return -1 * self.forward
+
+    @property
+    def up(self):
+        """
+        Get the up vector of the transform.
+        That is
+        :return: The "up" direction for this transform.
+        """
+        return self.find_independent((0, 1, 0))
+
+    @property
+    def down(self):
+        return -1 * self.up
+
+    @property
+    def right(self):
+        return self.find_independent((0, 0, 1))
+
+    @property
+    def left(self):
+        return -1 * self.right
+
+    def find_relative(self, pose):
+        """
+        Convert the given pose to be relative to this pose.
+        This is not commutative p1.find_relative(p2) != p2.find_relative(p1)
+
+        See Robotics: Vision and Control p 55-56 for the source of this math
+
+        :param pose: The world pose to convert
+        :return: A pose object relative to this pose
+        """
+        # Remember, the pose matrix gives the position in world coordinates from a local position,
+        # So to find the world position, we have to reverse it
+        if isinstance(pose, Transform):
+            inv_rot = tf.quaternions.qinverse((self._qw, self._qx, self._qy, self._qz))
+            loc = tf.quaternions.rotate_vector(pose.location - self.location, inv_rot)
+            rot = tf.quaternions.qmult(inv_rot, (pose._qw, pose._qx, pose._qy, pose._qz))
+            return Transform(location=loc, rotation=rot, w_first=True)
+        elif len(pose) >= 3:
+            return tf.quaternions.rotate_vector(pose - self.location,
+                                                tf.quaternions.qinverse((self._qw, self._qx, self._qy, self._qz)))
+        else:
+            raise TypeError('find_relative needs to transform a point or pose')
+
+    def find_independent(self, pose):
+        """
+        Convert a pose to world coordinates.
+        Remember, pose is like a stack, so the returned pose will be relative to whatever
+        this pose is relative to.
+
+        :param pose: A pose relative to this pose, as a Transform or just as a point (any length-3 indexable object)
+        :return: A pose relative to whatever this pose is relative to, and independent of this pose
+        """
+        # REMEMBER: pre-multiplying by the transformation matrix gives the world pose from the local
+        if isinstance(pose, Transform):
+            loc = self.location + tf.quaternions.rotate_vector(pose.location, (self._qw, self._qx, self._qy, self._qz))
+            rot = tf.quaternions.qmult((self._qw, self._qx, self._qy, self._qz),
+                                       (pose._qw, pose._qx, pose._qy, pose._qz))
+            return Transform(location=loc, rotation=rot, w_first=True)
+        elif len(pose) >= 3:
+            return tf.quaternions.rotate_vector(pose, (self._qw, self._qx, self._qy, self._qz)) + self.location
+        else:
+            raise TypeError('find_independent needs to transform a point or pose')
