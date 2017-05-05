766331425bf3e2f414c12b0a9f50cb84c789d0c7
= <=>
2017-05-05 18:00:13 +1000

    Move trial runner from core

--- batch_analysis/tests/test_trial_runner.py
+++ batch_analysis/tests/test_trial_runner.py
@@ -1,15 +1,8 @@
-import unittest, unittest.mock
-import core.trial_runner as runner
-import core.system
+import unittest
+import unittest.mock
 import core.image_source
-
-
-class MockSystem(core.system.VisionSystem):
-    pass
-
-
-class MockImageSource(core.image_source.ImageSource):
-    pass
+import core.system
+import batch_analysis.trial_runner as runner
 
 
 class TestTrialRunner(unittest.TestCase):
@@ -24,11 +17,14 @@ class TestTrialRunner(unittest.TestCase):
         self._image_source = unittest.mock.Mock(spec=core.image_source.ImageSource)
         self._image_source._image_count = 0
         self._image_source._image = unittest.mock.Mock()
-        def get_next_image(self=self._image_source):
-            self._image_count += 1
-            return self._image
-        def is_complete(self=self._image_source):
-            return self._image_count >= 10
+
+        def get_next_image(self_=self._image_source):
+            self_._image_count += 1
+            return self_._image
+
+        def is_complete(self_=self._image_source):
+            return self_._image_count >= 10
+
         self._image_source.get_next_image.side_effect = get_next_image
         self._image_source.is_complete.side_effect = is_complete
 
@@ -40,7 +36,7 @@ class TestTrialRunner(unittest.TestCase):
     def test_run_system_calls_trial_functions_in_order(self):
         runner.run_system_with_source(self._system, self._image_source)
         mock_calls = self._system.mock_calls
-        self.assertEqual(13, len(mock_calls)) # is_appropriate; begin; 10 process image calls; end
+        self.assertEqual(13, len(mock_calls))  # is_appropriate; begin; 10 process image calls; end
         self.assertEqual('start_trial', mock_calls[1][0])
         for i in range(10):
             self.assertEqual('process_image', mock_calls[2 + i][0])
