cdff8316c7c374d2400a06cdf2acf1276ef13697
John Skinner <jskinn7@gmail.com>
2017-05-09 17:07:50 +1000

    Add executables to perform all the basic tasks.

--- reset_permissions.sh
+++ reset_permissions.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # Reset the file permissions for all the relevant files
-chmod +x reset_permissions.sh add_initial_entities.py
+chmod +x reset_permissions.sh
 chmod -x README.md .gitignore
 find . -name "*.py" -exec chmod -x {} \;
-
+chmod +x add_initial_entities.py
+chmod +x task_train_system.py
+chmod +x task_run_system.py
+chmod +x task_benchmark_result.py
+chmod +x task_compare_trials.py
+chmod +x task_compare_benchmark_results.py
