92c17354f397704d9fb05c50120424a6eb16b4b6
= <=>
2017-07-18 23:52:19 +1000

    Make the scheduler executable

--- reset_permissions.sh
+++ reset_permissions.sh
@@ -4,6 +4,7 @@ chmod +x reset_permissions.sh
 chmod -x README.md .gitignore
 find . -name "*.py" -exec chmod -x {} \;
 chmod +x add_initial_entities.py
+chmod +x scheduler.py
 chmod +x task_train_system.py
 chmod +x task_run_system.py
 chmod +x task_benchmark_result.py
