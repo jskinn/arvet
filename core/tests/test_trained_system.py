d8eba3d4f1b49df2d1c3029a731e30bad7a82f63
= <=>
2017-07-19 21:35:01 +1000

    Rearrage the way trained systems work, to better match the process we will use

--- core/tests/test_trained_system.py
+++ core/tests/test_trained_system.py
@@ -1,8 +1,59 @@
 import unittest
+import unittest.mock as mock
+import abc
 import bson.objectid
 import database.tests.test_entity
 import util.dict_utils as du
 import core.trained_system
+
+
+class TrainerContract(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def make_instance(self, *args, **kwargs):
+        """
+        Make an instance of the system under test, with some default arguments
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        pass
+
+    def make_mock_trainee(self):
+        """
+        Create a mock trainee.
+        Subclasses should override this to make sure that the mock trainee
+        can be trained by the trainer.
+        :return: An instance of VisionSystemTrainee
+        """
+        return mock.create_autospec(core.trained_system.VisionSystemTrainee)
+
+    def test_calls_trainee_in_correct_order(self):
+        mock_trainee = self.make_mock_trainee()
+        subject = self.make_instance()
+        subject.train_vision_system(mock_trainee)
+
+        # Make sure each of the required methods is called at least once
+        self.assertTrue(mock_trainee.start_training.called)
+        self.assertTrue(mock_trainee.train_with_image.called)
+        self.assertTrue(mock_trainee.finish_training.called)
+
+        is_training = False
+        open_training = 0
+        for call in mock_trainee.method_calls:
+            if is_training:
+                self.assertNotEqual('start_training', call[0])  # No nested calls to start_training
+                if call[0] == 'finish_training':
+                    is_training = False
+                    open_training -= 1
+            else:
+                self.assertNotEqual('train_with_image', call[0])    # Can't train_with_image, since we're not training
+                self.assertNotEqual('finish_training', call[0])     # Can't finish without starting
+                if call[0] == 'start_training':
+                    is_training = True
+                    open_training += 1
+        self.assertFalse(is_training)
+        self.assertEqual(0, open_training)  # Make sure we start and finish training the same number of times
 
 
 class MockTrainedSystem(core.trained_system.TrainedVisionSystem):
@@ -41,7 +92,11 @@ class TestTrainedSystem(database.tests.test_entity.EntityContract, unittest.Test
                 bson.objectid.ObjectId(),
                 bson.objectid.ObjectId(),
                 bson.objectid.ObjectId()
-            ]
+            ],
+            'training_settings': {
+                'A': 'Yes',
+                'but really?': True
+            }
         })
         return MockTrainedSystem(*args, **kwargs)
 
@@ -58,3 +113,4 @@ class TestTrainedSystem(database.tests.test_entity.EntityContract, unittest.Test
         self.assertEqual(trial_result1.identifier, trial_result2.identifier)
         self.assertEqual(trial_result1.vision_system_trainer, trial_result2.vision_system_trainer)
         self.assertEqual(trial_result1.training_image_sources, trial_result2.training_image_sources)
+        self.assertEqual(trial_result1.training_settings, trial_result2.training_settings)
--- core/trained_system.py
+++ core/trained_system.py
@@ -5,12 +5,66 @@ import core.system
 
 class VisionSystemTrainer(database.entity.Entity, metaclass=database.entity.AbstractEntityMetaclass):
     """
+    This class manages image sources, and how they are used for training.
+    This includes handling multiple image sources, shuffling,
+    presentation order, sampling, and whatnot
+
+    There are two key styles here, systems that require image sequences, and systems that require single images.
+    Training on single images does things like shuffle the images, present the same images more than once,
+    etc, etc.
+    Training on image sequences does similar things, but at the image source level instead,
+    presenting each source in order, but potentially randomizing the order the sources is presented.
+    They are distinguished by the required_training_type property of the vision system trainee.
+
+    Instances of this class must be entities, with particular settings, so that the experiments can load them.
+    """
+
+    @abc.abstractmethod
+    def can_train_trainee(self, potential_trainee):
+        """
+        Is this trainer set-up to train this particular system trainee.
+        Should check that it matches the required training type,
+        and that it has at least one appropriate image source.
+        :param potential_trainee: A VisionSystemTrainee that
+        :return:
+        """
+        pass
+
+    @abc.abstractmethod
+    def train_vision_system(self, vision_system_trainee):
+        """
+        Train a new vision system using the given trainee
+        :param vision_system_trainee: The builder-type trainee to feed training images to.
+        :return:
+        """
+        pass
+
+
+class VisionSystemTrainee(database.entity.Entity, metaclass=database.entity.AbstractEntityMetaclass):
+    """
     A separate class like a builder for training Vision Systems.
+    It holds all the algorithm specific state during training,
+    as opposed to the training approach, which is provided by the Trainer (above),
+    and is probably more generic.
+
     This breaks some awkward dependencies with having the system class
     also be responsible for training.
     
-    Like vision systems, all trainers must be entities so that the database can load them.
+    All trainees must be entities so that the job system can load them when running training tasks.
+    However, trainees should not store state generated during training,
+    that is the job of the TrainedVisionSytem returned by finish_training
     """
+
+    @property
+    @abc.abstractmethod
+    def required_training_type(self):
+        """
+        What kind of training does this system require?
+
+        :return: NON_SEQUENTIAL for systems trained on random images, and SEQUENTIAL for systems trained on video
+        :rtype core.sequence_type.ImageSequenceType:
+        """
+        pass
 
     @abc.abstractmethod
     def is_image_source_appropriate(self, image_source):
@@ -26,29 +80,29 @@ class VisionSystemTrainer(database.entity.Entity, metaclass=database.entity.Abst
         pass
 
     @abc.abstractmethod
-    def start_training(self, training_image_source_id):
+    def start_training(self, num_images=None):
         """
         Start training the system.
         After calling this, train_with_image should be called with images from the image source.
-        If we want to train on image data from multiple image sources, call start_training
-        for each image source, and only call finish_training when all image sources are complete
-        
-        :param training_image_source_id: ObjectId
+        Only call start_training once, and call finish_training when all image sources are complete.
+        Provides the expected number of images if available, for scaling learning rates and such.
+        :param num_images: The anticipated total number of images that will be provided, if available.
         :return: void
         """
         pass
 
     @abc.abstractmethod
-    def train_with_image(self, image):
+    def train_with_image(self, image, index=None):
         """
         Train the system with a particular image
-        :param image: 
+        :param image: The image to train with, and Image object
+        :param index: A timestamp or index into the training images, for scaling learning rates
         :return: void
         """
         pass
 
     @abc.abstractmethod
-    def finish_training(self):
+    def finish_training(self, trainer_id, image_source_ids=None, training_settings=None):
         """
         Finish training, producing a new vision system instance,
         which will be saved in the database as a new vision system to be tested.
@@ -56,35 +110,29 @@ class VisionSystemTrainer(database.entity.Entity, metaclass=database.entity.Abst
         If the training method is not sequential, requiring all images to train,
         then train_with_image should simply accumulate images, and this method
         should actually perform the training.
-        
-        :return: TrainedVisionSystem  
+
+        :param trainer_id: The id of the VisionSystemTrainer that provided the images
+        :param image_source_ids: The ids of the image sources used by the trainer, if available
+        :param training_settings: The settings of the trainer. Optional.
+        :return: TrainedVisionSystem A completed TrainedVisionSystem, with state created from the given images.
         """
         pass
-
-    @classmethod
-    @abc.abstractmethod
-    def get_training_dataset_criteria(cls):
-        """
-        Get search criteria for finding datasets that can be used by this system for training.
-        The scheduling process with try and train vision systems using all image sources that match this criteria.
-
-        :return: A set of criteria for finding datasets that match, in mongodb query format
-        :rtype: dict
-        """
-        return {}
 
 
 class TrainedVisionSystem(core.system.VisionSystem, metaclass=database.entity.AbstractEntityMetaclass):
     """
     A subclass of a vision system that are produced by training.
-    Instances of this class are returned by VisionSystemTrainers.
-    The only difference here is that they have references back to the image 
+    Instances of this class are built by VisionSystemTrainers using VisionSystemTrainees.
+    The only difference here is that they have references back to the image sources they were trained with
+    #TODO: Once we've sorted out universal ids for images (db or generated), store those for all training images
     """
 
-    def __init__(self, vision_system_trainer, training_image_source_ids, id_=None, **kwargs):
+    def __init__(self, vision_system_trainer, training_image_source_ids=None, training_settings=None,
+                 id_=None, **kwargs):
         super().__init__(id_=id_, **kwargs)
         self._trainer = vision_system_trainer
         self._training_image_sources = training_image_source_ids
+        self._training_settings = training_settings
 
     @property
     def vision_system_trainer(self):
@@ -102,6 +150,14 @@ class TrainedVisionSystem(core.system.VisionSystem, metaclass=database.entity.Ab
         """
         return self._training_image_sources
 
+    @property
+    def training_settings(self):
+        """
+        Get the settings used to train this system
+        :return:
+        """
+        return self._training_settings
+
     def serialize(self):
         """
         Serialize the entity to a dict format, that can be saved to the database
@@ -110,6 +166,7 @@ class TrainedVisionSystem(core.system.VisionSystem, metaclass=database.entity.Ab
         serialized = super().serialize()
         serialized['vision_system_trainer'] = self.vision_system_trainer
         serialized['training_image_sources'] = self.training_image_sources
+        serialized['training_settings'] = self._training_settings
         return serialized
 
     @classmethod
@@ -118,4 +175,6 @@ class TrainedVisionSystem(core.system.VisionSystem, metaclass=database.entity.Ab
             kwargs['vision_system_trainer'] = serialized_representation['vision_system_trainer']
         if 'training_image_sources' in serialized_representation:
             kwargs['training_image_source_ids'] = serialized_representation['training_image_sources']
+        if 'training_settings' in serialized_representation:
+            kwargs['training_settings'] = serialized_representation['training_settings']
         return super().deserialize(serialized_representation, db_client, **kwargs)
