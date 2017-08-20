f2b97c6294b2ecc96c24c52547c7d6ceb03caa70
= <=>
2017-08-21 08:54:19 +1000

    Build a structure for holding the experimental results. Plotting is still a mess.

--- experiments/visual_slam/visual_slam_experiment.py
+++ experiments/visual_slam/visual_slam_experiment.py
@@ -1,5 +1,7 @@
 import os
 import glob
+import numpy as np
+import pandas
 import matplotlib.pyplot as pyplot
 from mpl_toolkits.mplot3d import Axes3D     # Necessary for 3D plots
 import util.database_helpers as dh
@@ -13,16 +15,18 @@ import benchmarks.trajectory_drift.trajectory_drift as traj_drift
 
 class VisualSlamExperiment(batch_analysis.experiment.Experiment):
 
-    def __init__(self, libviso_system=None, orbslam_monocular_system=None, orbslam_stereo_system=None,
-                 orbslam_rgbd_system=None, benchmark_rpe=None, benchmark_trajectory_drift=None, id_=None):
+    def __init__(self, libviso_system=None, orbslam_map=None, benchmark_rpe=None, benchmark_trajectory_drift=None,
+                 datasets=None, trajectory_map=None, trial_list=None, result_list=None, id_=None):
         super().__init__(id_=id_)
         self._libviso_system = libviso_system
-        self._orbslam_monocular = orbslam_monocular_system
-        self._orbslam_stereo = orbslam_stereo_system
-        self._orbslam_rgbd = orbslam_rgbd_system
+        self._orbslam_map = orbslam_map if orbslam_map is not None else {}
         self._benchmark_rpe = benchmark_rpe
         self._benchmark_trajectory_drift = benchmark_trajectory_drift
-        self._datasets = set()
+        self._datasets = set(datasets) if datasets is not None else set()
+        self._trajectory_map = trajectory_map if trajectory_map is not None else []
+        self._trial_list = trial_list if trial_list is not None else []
+        self._result_list = result_list if result_list is not None else []
+        self._image_source_cache = {}
 
     def do_imports(self, task_manager, db_client):
         """
@@ -42,7 +46,6 @@ class VisualSlamExperiment(batch_analysis.experiment.Experiment):
                 expected_duration='4:00:00'
             )
             if import_dataset_task.is_finished:
-                # TODO: Map out the datasets by world, trajectory, and quality change. Needs data structure
                 self._datasets.add(import_dataset_task.result)
             else:
                 task_manager.do_task(import_dataset_task)
@@ -59,24 +62,27 @@ class VisualSlamExperiment(batch_analysis.experiment.Experiment):
                 base=30
             ))
             self._set_property('libviso', self._libviso_system)
-        if self._orbslam_monocular is None:
-            self._orbslam_monocular = dh.add_unique(db_client.system_collection, orbslam2.ORBSLAM2(
-                vocabulary_file='/opt/ORBSLAM2/Vocabulary/ORBvoc.txt',
-                settings={}, mode=orbslam2.SensorMode.MONOCULAR, resolution=(1280, 720)
-            ))
-            self._set_property('orbslam_monocular', self._orbslam_monocular)
-        if self._orbslam_stereo is None:
-            self._orbslam_stereo = dh.add_unique(db_client.system_collection, orbslam2.ORBSLAM2(
-                vocabulary_file='/opt/ORBSLAM2/Vocabulary/ORBvoc.txt',
-                settings={}, mode=orbslam2.SensorMode.STEREO, resolution=(1280, 720)
-            ))
-            self._set_property('orbslam_stereo', self._orbslam_stereo)
-        if self._orbslam_rgbd is None:
-            self._orbslam_rgbd = dh.add_unique(db_client.system_collection, orbslam2.ORBSLAM2(
-                vocabulary_file='/opt/ORBSLAM2/Vocabulary/ORBvoc.txt',
-                settings={}, mode=orbslam2.SensorMode.RGBD, resolution=(1280, 720)
-            ))
-            self._set_property('orbslam_rgbd', self._orbslam_rgbd)
+        # ORBSLAM2 - 12 variants for parameter sweep
+        settings_list = [
+            (sensor_mode, n_features, scale_factor, resolution)
+            for sensor_mode in {orbslam2.SensorMode.MONOCULAR, orbslam2.SensorMode.STEREO, orbslam2.SensorMode.RGBD}
+            for n_features in {1000, 2000}
+            for scale_factor in {1.2}
+            for resolution in {(1280, 720), (640, 480)}
+        ]
+        for settings in settings_list:
+            settings_key = (settings[0].value, settings[1], settings[2], settings[3][0], settings[3][1])
+            if settings_key not in self._orbslam_map:
+                self._orbslam_map[settings_key] = dh.add_unique(db_client.system_collection, orbslam2.ORBSLAM2(
+                    vocabulary_file='/opt/ORBSLAM2/Vocabulary/ORBvoc.txt',
+                    mode=settings[0],
+                    settings={
+                        'ORBextractor': {
+                            'nFeatures': settings[1],
+                            'scaleFactor': settings[2]
+                        }
+                    }, resolution=settings[3]
+                ))
 
         # Create and store the benchmarks for camera trajectories
         # Just using the default settings for now
@@ -99,14 +105,10 @@ class VisualSlamExperiment(batch_analysis.experiment.Experiment):
             self._set_property('benchmark_trajectory_drift', self._benchmark_trajectory_drift)
 
     def schedule_tasks(self, task_manager, db_client):
-        # TODO: Flatten the map of image sources to a list we can loop over
-        test_image_sources = [dh.load_object(db_client, db_client.image_source_collection, source_id)
-                              for source_id in self._datasets]
-        systems = [
-            dh.load_object(db_client, db_client.system_collection, self._libviso_system),
-            dh.load_object(db_client, db_client.system_collection, self._orbslam_monocular),
-            dh.load_object(db_client, db_client.system_collection, self._orbslam_stereo),
-            dh.load_object(db_client, db_client.system_collection, self._orbslam_rgbd)
+        libviso_system = dh.load_object(db_client, db_client.system_collection, self._libviso_system)
+        orbslam_systems = [
+            dh.load_object(db_client, db_client.system_collection, system_id)
+            for system_key, system_id in self._orbslam_map.items()
         ]
         benchmarks = [
             dh.load_object(db_client, db_client.benchmarks_collection, self._benchmark_rpe),
@@ -114,25 +116,48 @@ class VisualSlamExperiment(batch_analysis.experiment.Experiment):
         ]
 
         # Schedule trials
-        trial_results = []
-        for system in systems:
-            for image_source in test_image_sources:
-                if system.is_image_source_appropriate(image_source):
+        for image_source_id in self._datasets:
+            image_source = self._load_image_source(db_client, image_source_id)
+            # Libviso2
+            if libviso_system.is_image_source_appropriate(image_source):
+                task = task_manager.get_run_system_task(
+                    system_id=libviso_system.identifier,
+                    image_source_id=image_source.identifier,
+                    expected_duration='4:00:00'
+                )
+                if task.is_finished:
+                    found = False
+                    for _, _, trial_result_id in self._trial_list:
+                        if trial_result_id == task.result:
+                            found = True
+                            break
+                    if not found:
+                        self._trial_list.append((image_source_id, self._libviso_system, task.result))
+                else:
+                    task_manager.do_task(task)
+
+            # ORBSLAM2
+            for orbslam_system in orbslam_systems:
+                if orbslam_system.is_image_source_appropriate(image_source):
                     task = task_manager.get_run_system_task(
-                        system_id=system.identifier,
+                        system_id=libviso_system.identifier,
                         image_source_id=image_source.identifier,
                         expected_duration='4:00:00'
                     )
                     if task.is_finished:
-                        # TODO: Store the trial results in a similar map to the datasets
-                        trial_results.append(dh.load_object(db_client, db_client.trials_collection, task.result))
+                        found = False
+                        for _, _, trial_result_id in self._trial_list:
+                            if trial_result_id == task.result:
+                                found = True
+                                break
+                        if not found:
+                            self._trial_list.append((image_source_id, orbslam_system.identifier, task.result))
                     else:
                         task_manager.do_task(task)
 
         # Benchmark results
-        # TODO: We need a similar structure for benchmark results as trial results
-        results = []
-        for trial_result in trial_results:
+        for image_source_id, system_id, trial_result_id in self._trial_list:
+            trial_result = dh.load_object(db_client, db_client.trials_collection, trial_result_id)
             for benchmark in benchmarks:
                 if benchmark.is_trial_appropriate(trial_result):
                     task = task_manager.get_benchmark_task(
@@ -141,7 +166,13 @@ class VisualSlamExperiment(batch_analysis.experiment.Experiment):
                         expected_duration='4:00:00'
                     )
                     if task.is_finished:
-                        results.append(task.result)
+                        found = False
+                        for _, _, _, benchmark_id in self._result_list:
+                            if benchmark_id == task.result:
+                                found = True
+                                break
+                        if not found:
+                            self._result_list.append((image_source_id, system_id, trial_result_id, task.result))
                     else:
                         task_manager.do_task(task)
 
@@ -152,6 +183,25 @@ class VisualSlamExperiment(batch_analysis.experiment.Experiment):
         :param db_client:
         :return:
         """
+        # Step 1: Group trials and results by trajectory, and by each image quality
+
+
+        trials_by_trajectory = [[] for _ in range(len(self._trajectory_map))]
+        results_by_trajectory = [[] for _ in range(len(self._trajectory_map))]
+
+        for image_source_id, system_id, trial_result_id in self._trial_list:
+            image_source = self._load_image_source(image_source_id, db_client)
+            metadata_key = self._make_metadata_key(image_source)
+            trials_by_trajectory[metadata_key[0]].append(
+                dh.load_object(db_client, db_client.trials_collection, trial_result_id)
+            )
+        for image_source_id, system_id, trial_result_id, benchmark_result_id in self._result_list:
+            image_source = self._load_image_source(image_source_id, db_client)
+            metadata_key = self._make_metadata_key(image_source)
+            results_by_trajectory[metadata_key[0]].append(
+                dh.load_object(db_client, db_client.trials_collection, benchmark_result_id)
+            )
+
         # TODO: Once we've grouped the trial results, plot them by group
         trial_results = []
         for trial_result_id in trial_results:
@@ -185,36 +235,107 @@ class VisualSlamExperiment(batch_analysis.experiment.Experiment):
             pyplot.subplots_adjust(top=0.95, right=0.99)
         pyplot.show()
 
-    def _get_representative_metadata(self, image_source_id, db_client):
-        # TODO: To sort the image sources, we need a way of getting aggregate metadata
-        # This function should somehow retrieve that given a dataset id.
-        images_list = db_client.image_sources_collection.find_one({'_id': image_source_id}, {
-            'images': True
-        })
-        images_list = sorted(images_list)
+    def _load_image_source(self, db_client, image_source_id):
+        if image_source_id not in self._image_source_cache:
+            self._image_source_cache[image_source_id] = dh.load_object(db_client, db_client.image_source_collection,
+                                                                       image_source_id)
+        return self._image_source_cache[image_source_id]
+
+    def _get_trajectory_id(self, trajectory):
+        """
+        Get an id number in the trajectory map matching the given trajectory.
+        If there are no matching trajectories, store it in the map and return the new index
+        :param trajectory: A trajectory, as timestamps to poses
+        :return: Integer trajectory id which is the index to the trajectory map
+        """
+        for idx, mapped_trajectory in enumerate(self._trajectory_map):
+            if are_matching_trajectories(trajectory, mapped_trajectory):
+                return idx
+        self._trajectory_map.append(trajectory)
+        return len(self._trajectory_map) - 1
+
+    def _make_metadata_key(self, dataset):
+        """
+        Make a vector of the properties we care about for associating results from a dataset
+        :param dataset: The image collection
+        :return:
+        """
+        trajectory = extract_trajectory(dataset)
+        trajectory_id = self._get_trajectory_id(trajectory)
+        first_image = dataset.get(0)
+        return (
+            trajectory_id,
+            first_image.metadata.source_type.value,
+            first_image.metadata.texture_mipmap_bias,
+            int(first_image.metadata.texture_mipmap_bias),
+            int(first_image.metadata.roughness_enabled),
+            first_image.metadata.geometry_decimation
+        )
 
     def serialize(self):
         serialized = super().serialize()
         serialized['libviso'] = self._libviso_system
-        serialized['orbslam_monocular'] = self._orbslam_monocular
-        serialized['orbslam_stereo'] = self._orbslam_stereo
-        serialized['orbslam_rgbd'] = self._orbslam_rgbd
+        serialized['orbslam_map'] = self._orbslam_map
         serialized['benchmark_rpe'] = self._benchmark_rpe
         serialized['benchmark_trajectory_drift'] = self._benchmark_trajectory_drift
+        serialized['datasets'] = list(self._datasets)
+        serialized['trajectory_map'] = [[(stamp, pose.serialize()) for stamp, pose in traj]
+                                        for traj in self._trajectory_map]
+        serialized['trial_list'] = self._trial_list
+        serialized['result_list'] = self._result_list
         return serialized
 
     @classmethod
     def deserialize(cls, serialized_representation, db_client, **kwargs):
         if 'libviso' in serialized_representation:
             kwargs['libviso_system'] = serialized_representation['libviso']
-        if 'orbslam_monocular' in serialized_representation:
-            kwargs['orbslam_monocular_system'] = serialized_representation['orbslam_monocular']
-        if 'orbslam_stereo' in serialized_representation:
-            kwargs['orbslam_stereo_system'] = serialized_representation['orbslam_stereo']
-        if 'orbslam_rgbd' in serialized_representation:
-            kwargs['orbslam_rgbd_system'] = serialized_representation['orbslam_rgbd']
+        if 'orbslam_map' in serialized_representation:
+            kwargs['orbslam_map'] = serialized_representation['orbslam_map']
         if 'benchmark_rpe' in serialized_representation:
             kwargs['benchmark_rpe'] = serialized_representation['benchmark_rpe']
         if 'benchmark_trajectory_drift' in serialized_representation:
             kwargs['benchmark_trajectory_drift'] = serialized_representation['benchmark_trajectory_drift']
+        if 'datasets' in serialized_representation:
+            kwargs['datasets'] = serialized_representation['datasets']
+        if 'trajectory_map' in serialized_representation:
+            kwargs['trajectory_map'] = serialized_representation['trajectory_map']
+        if 'trial_list' in serialized_representation:
+            kwargs['trial_list'] = serialized_representation['trial_list']
+        if 'result_list' in serialized_representation:
+            kwargs['result_list'] = serialized_representation['result_list']
         return super().deserialize(serialized_representation, db_client, **kwargs)
+
+
+def extract_trajectory(image_source):
+    """
+    Extract a trajectory from an image source, which will almost always be an image collection.
+    :param image_source: The image source to extract from
+    :return: The ground-truth camera trajectory, as a dict of timestamp to pose
+    """
+    trajectory = {}
+    image_source.begin()
+    while not image_source.is_complete():
+        image, timestamp = image_source.get_next_image()
+        trajectory[timestamp] = image.metadata.camera_pose
+    return trajectory
+
+
+def are_matching_trajectories(trajectory1, trajectory2, location_threshold=10, time_threshold=1):
+    """
+    For the purposes of this experiment, do two image sources follow the same trajectory.
+    Because of the simulation, we expect trajectories to be either exactly the same or completely different
+    :param trajectory1: map of timestamps to poses
+    :param trajectory2: map of timestamps to poses
+    :param location_threshold: The maximum location error before trajectories are considered different. Default 10.
+    :param time_threshold: The maximum difference in timestamp for matching trajectories. Default 1.
+    :return:
+    """
+    timestamp_matches = util.associate.associate(trajectory1, trajectory2, offset=0, max_difference=time_threshold)
+    if float(len(timestamp_matches)) < 0.9 * min(len(trajectory1), len(trajectory2)):
+        return False
+    square_threshold = location_threshold * location_threshold
+    for traj1_stamp, traj2_stamp in timestamp_matches:
+        dist = trajectory1[traj1_stamp].location - trajectory2[traj2_stamp].location
+        if np.dot(dist, dist) > square_threshold:
+            return False
+    return True
