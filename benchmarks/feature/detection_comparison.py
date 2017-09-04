360b61b46f9dc94bf21279fdb851053fc0567a8e
John Skinner <jskinn7@gmail.com>
2017-09-04 11:19:52 +1000

    Add a comparison benchmark for finding changed image features.

---
+++ benchmarks/feature/detection_comparison.py
@@ -0,0 +1,129 @@
+import bson
+import core.trial_comparison
+import core.benchmark
+import trials.feature_detection.feature_detector_result as detector_result
+
+
+class FeatureDetectionComparison(core.trial_comparison.TrialComparison):
+
+    def __init__(self, acceptable_radius=4, id_=None):
+        super().__init__(id_=id_)
+        self._acceptable_radius = acceptable_radius
+
+    def is_trial_appropriate(self, trial_result):
+        return isinstance(detector_result.FeatureDetectorResult, trial_result)
+
+    def compare_trial_results(self, trial_result, reference_trial_result):
+        if not isinstance(trial_result, detector_result.FeatureDetectorResult):
+            return core.benchmark.FailedBenchmark(self.identifier, trial_result.identifier,
+                                                  "{0} is not a FeatureDetectorResult".format(trial_result.identifier))
+        if not isinstance(reference_trial_result, detector_result.FeatureDetectorResult):
+            return core.benchmark.FailedBenchmark(self.identifier, reference_trial_result.identifier,
+                                                  "{0} is not a FeatureDetectorResult".format(
+                                                      reference_trial_result.identifier))
+        reference_feature_points = reference_trial_result.keypoints
+        base_feature_points = trial_result.keypoints
+        image_ids = set(reference_feature_points.keys()) & set(base_feature_points.keys())
+        point_matches = {}
+        missing_trial = {}
+        missing_reference = {}
+        for image_id in image_ids:
+            points1 = {point.pt for point in trial_result.keypoints[image_id]}
+            points2 = {point.pt for point in reference_trial_result.keypoints[image_id]}
+            potential_matches = sorted(
+                (point_dist(point1, point2), point1, point2)
+                for point1 in points1
+                for point2 in points2
+                if point_dist(point1, point2) < self._acceptable_radius)
+            point_matches[image_id] = []
+            for match in potential_matches:
+                coords1 = match[1].pt
+                coords2 = match[2].pt
+                if coords1 in points1 and coords2 in points2:
+                    points1.remove(coords1)
+                    points2.remove(coords2)
+                    point_matches[image_id].append((match[1], match[2]))
+            missing_trial[image_id] = list(points1)
+            missing_reference[image_id] = list(points2)
+        return FeatureDetectionComparisonResult(
+            benchmark_id=self.identifier,
+            trial_result_id=trial_result.identifier,
+            reference_id=reference_trial_result.identifier,
+            point_matches=point_matches,
+            missing_trial=missing_trial,
+            missing_reference=missing_reference)
+
+    def serialize(self):
+        serialized = super().serialize()
+        serialized['acceptable_radius'] = self._acceptable_radius
+        return serialized
+
+    @classmethod
+    def deserialize(cls, serialized_representation, db_client, **kwargs):
+        if 'acceptable_radius' in serialized_representation:
+            kwargs['acceptable_radius'] = serialized_representation['acceptable_radius']
+        return super().deserialize(serialized_representation, db_client, **kwargs)
+
+    @classmethod
+    def get_trial_requirements(cls):
+        pass
+
+
+class FeatureDetectionComparisonResult(core.trial_comparison.TrialComparisonResult):
+
+    def __init__(self, benchmark_id, trial_result_id, reference_id, point_matches, missing_trial, missing_reference,
+                 id_=None, **kwargs):
+        kwargs['success'] = True
+        super().__init__(
+            benchmark_id=benchmark_id,
+            trial_result_id=trial_result_id,
+            reference_id=reference_id,
+            id_=id_, **kwargs)
+        self._point_matches = point_matches
+        self._missing_trial = missing_trial
+        self._missing_reference = missing_reference
+
+    def compute_intersection_over_union(self):
+        """
+        Provide a score for each image, intersection over union of detected feature points
+        between the reference and trial images.
+        This will be 1 if every point is re-detected with none left over.
+        Detecting extra points, or failing to detect points reduce the ratio.
+        :return:
+        """
+        return {image_id: len(self._point_matches[image_id]) / (len(self._point_matches[image_id]) +
+                                                                len(self._missing_trial[image_id]) +
+                                                                len(self._missing_reference[image_id]))
+                for image_id in self._point_matches.keys()}
+
+    def get_image_ids(self):
+        return self._point_matches.keys()
+
+    def get_for_image(self, image_id):
+        if image_id in self._point_matches and image_id in self._missing_trial and image_id in self._missing_reference:
+            return self._point_matches[image_id], self._missing_trial[image_id], self._missing_reference[image_id]
+
+    def serialize(self):
+        serialized = super().serialize()
+        serialized['point_matches'] = {str(img_id): matches for img_id, matches in self._point_matches.items()}
+        serialized['missing_trial'] = {str(img_id): points for img_id, points in self._missing_trial.items()}
+        serialized['missing_reference'] = {str(img_id): points for img_id, points in self._missing_reference.items()}
+        return serialized
+
+    @classmethod
+    def deserialize(cls, serialized_representation, db_client, **kwargs):
+        if 'point_matches' in serialized_representation:
+            kwargs['point_matches'] = {bson.ObjectId(str_id): matches for str_id, matches
+                                       in serialized_representation['point_matches'].items()}
+        if 'missing_trial' in serialized_representation:
+            kwargs['missing_trial'] = {bson.ObjectId(str_id): points for str_id, points
+                                       in serialized_representation['missing_trial'].items()}
+        if 'missing_reference' in serialized_representation:
+            kwargs['missing_reference'] = {bson.ObjectId(str_id): points for str_id, points
+                                           in serialized_representation['missing_reference'].items()}
+        return super().deserialize(serialized_representation, db_client, **kwargs)
+
+
+def point_dist(point1, point2):
+    diff = (point1[0] - point2[0], point1[1] - point2[1])
+    return diff[0] * diff[0] + diff[1] * diff[1]
---
+++ benchmarks/feature/tests/test_detection_comparison.py
@@ -0,0 +1,100 @@
+import numpy as np
+import unittest
+import bson
+import cv2
+import util.dict_utils as du
+import util.transform as tf
+import database.tests.test_entity as entity_test
+import core.sequence_type
+import core.trial_comparison
+import trials.feature_detection.feature_detector_result as detector_result
+import benchmarks.feature.detection_comparison as detection_comp
+
+
+class TestATEBenchmarkComparison(entity_test.EntityContract, unittest.TestCase):
+
+    def get_class(self):
+        return detection_comp.FeatureDetectionComparison
+
+    def make_instance(self, *args, **kwargs):
+        return detection_comp.FeatureDetectionComparison(*args, **kwargs)
+
+    def assert_models_equal(self, benchmark1, benchmark2):
+        """
+        Helper to assert that two benchmarks are equal
+        :param benchmark1: FeatureDetectionComparison
+        :param benchmark2: FeatureDetectionComparison
+        :return:
+        """
+        if (not isinstance(benchmark1, detection_comp.FeatureDetectionComparison) or
+                not isinstance(benchmark2, detection_comp.FeatureDetectionComparison)):
+            self.fail('object was not a FeatureDetectionComparison')
+        self.assertEqual(benchmark1.identifier, benchmark2.identifier)
+        self.assertEqual(benchmark1._acceptable_radius, benchmark2._acceptable_radius)
+
+    def test_comparison_returns_comparison_result(self):
+        random = np.random.RandomState(1687)
+        image_id = bson.ObjectId()
+        trial_result1 = detector_result.FeatureDetectorResult(
+            system_id=bson.ObjectId(),
+            keypoints={image_id: [cv2.KeyPoint(x=p[0], y=p[1], _size=1) for p in random.randint(0, 300, (10, 2))]},
+            timestamps={10: image_id},
+            camera_poses={image_id: tf.Transform()},
+            sequence_type=core.sequence_type.ImageSequenceType.NON_SEQUENTIAL,
+            system_settings={}
+        )
+        trial_result2 = detector_result.FeatureDetectorResult(
+            system_id=bson.ObjectId(),
+            keypoints={image_id: [cv2.KeyPoint(x=p[0], y=p[1], _size=1) for p in random.randint(0, 300, (10, 2))]},
+            timestamps={10: image_id},
+            camera_poses={image_id: tf.Transform()},
+            sequence_type=core.sequence_type.ImageSequenceType.NON_SEQUENTIAL,
+            system_settings={}
+        )
+
+        comparison_benchmark = detection_comp.FeatureDetectionComparison()
+        comparison_result = comparison_benchmark.compare_trial_results(trial_result1, trial_result2)
+        self.assertIsInstance(comparison_result, core.trial_comparison.TrialComparisonResult)
+        self.assertEqual(comparison_benchmark.identifier, comparison_result.benchmark)
+        self.assertEqual(trial_result1.identifier, comparison_result.trial_result)
+        self.assertEqual(trial_result2.identifier, comparison_result.reference_trial_result)
+
+
+class TestATEBenchmarkComparisonResult(entity_test.EntityContract, unittest.TestCase):
+
+    def get_class(self):
+        return detection_comp.FeatureDetectionComparisonResult
+
+    def make_instance(self, *args, **kwargs):
+        kwargs = du.defaults(kwargs, {
+            'benchmark_id': bson.ObjectId(),
+            'trial_result_id': bson.ObjectId(),
+            'reference_id': bson.ObjectId(),
+            'point_matches': {bson.ObjectId(): [((p[0], p[1]), (p[2], p[3]))
+                                                for p in np.random.randint(0, 300, (10, 4))]
+                              for _ in range(3)},
+            'missing_trial': {bson.ObjectId(): [(p[0], p[1]) for p in np.random.randint(0, 300, (10, 2))]
+                              for _ in range(3)},
+            'missing_reference': {bson.ObjectId(): [(p[0], p[1]) for p in np.random.randint(0, 300, (10, 2))]
+                                  for _ in range(3)}
+        })
+        return detection_comp.FeatureDetectionComparisonResult(*args, **kwargs)
+
+    def assert_models_equal(self, benchmark1, benchmark2):
+        """
+        Helper to assert that two benchmark results are equal
+        :param benchmark1: FeatureDetectionComparisonResult
+        :param benchmark2: FeatureDetectionComparisonResult
+        :return:
+        """
+        if (not isinstance(benchmark1, detection_comp.FeatureDetectionComparisonResult) or
+                not isinstance(benchmark2, detection_comp.FeatureDetectionComparisonResult)):
+            self.fail('object was not a FeatureDetectionComparisonResult')
+        self.assertEqual(benchmark1.identifier, benchmark2.identifier)
+        self.assertEqual(benchmark1.success, benchmark2.success)
+        self.assertEqual(benchmark1.benchmark, benchmark2.benchmark)
+        self.assertEqual(benchmark1.trial_result, benchmark2.trial_result)
+        self.assertEqual(benchmark1.reference_trial_result, benchmark2.reference_trial_result)
+        self.assertEqual(benchmark1._point_matches, benchmark2._point_matches)
+        self.assertEqual(benchmark1._missing_trial, benchmark2._missing_trial)
+        self.assertEqual(benchmark1._missing_reference, benchmark2._missing_reference)
