d9f25a3eb22131d20604814b090d3e2ab9864d04
John Skinner <jskinn7@gmail.com>
2017-12-07 16:25:55 +1000

    Create an image_utils module to abstract how image are loaded and modified.

---
+++ arvet/util/image_utils.py
@@ -0,0 +1,75 @@
+# Copyright (c) 2017, John Skinner
+import os
+import typing
+import numpy as np
+import PIL.Image
+
+
+def read_colour(filename: str) -> typing.Union[None, np.ndarray]:
+    """
+    Load an image from file, however we can.
+    :param filename:
+    :return:
+    """
+    if os.path.isfile(filename):
+        try:
+            pil_image = PIL.Image.open(filename)
+        except IOError:
+            pil_image = None
+        return np.array(pil_image) if pil_image is not None else None
+    return None
+
+
+def read_depth(filename: str) -> typing.Union[None, np.ndarray]:
+    """
+    Load a depth image from a file.
+    Depth images are single channel with higher bit depth (usually 16-bit)
+    :param filename:
+    :return:
+    """
+    return read_colour(filename)
+
+
+def convert_to_grey(image_data: np.ndarray) -> np.ndarray:
+    """
+    Convert an image to greyscale
+    :param image_data: The three channel image data, in RGB format
+    :return: The image in greyscale, single channel
+    """
+    if len(image_data.shape) > 2 and image_data.shape[2] == 3:
+        pil_image = PIL.Image.fromarray(image_data)
+        return np.array(pil_image.convert('L'))
+    return image_data
+
+
+def get_bounding_box(image_data: np.ndarray) -> typing.Union[typing.Tuple[int, int, int, int], None]:
+    """
+    Get the bounding box of the non-zero values in a particular image.
+    Call this on a logical image or mask.
+    :param image_data: The image data, as a numpy array.
+    :return: A tuple of (left, upper, right, lower) pixel coordinates, or None if the image is empty
+    """
+    if image_data.dtype == np.bool:
+        # PIL won't handle logical np arrays, it expects everything as uint8
+        image_data = image_data.astype(dtype=np.uint8)
+    pil_image = PIL.Image.fromarray(image_data)
+    return pil_image.getbbox()
+
+
+def show_image(image_data: np.ndarray, window_name: str = 'temp') -> None:
+    """
+    Display a particular image to the screen.
+    Useful for visualization of results. Note that this cannot handle video or successive frames.
+    :param image_data: The image data to display, as an ndarray
+    :param window_name: The name of the window, default 'temp'
+    :return:
+    """
+    # PIL basically only handles integer images, so convert to that.
+    if (image_data.dtype == np.float or image_data.dtype == np.float16 or
+            image_data.dtype == np.float32 or image_data.dtype == np.float64 or
+            image_data.dtype == np.double) and image_data.max() <= 1:
+        image_data = (255 * image_data).astype(dtype=np.uint8)
+    elif image_data.dtype == np.bool:
+        image_data = image_data.astype(dtype=np.uint8)
+    pil_image = PIL.Image.fromarray(image_data)
+    pil_image.show(window_name)
---
+++ arvet/util/tests/test_image_utils.py
@@ -0,0 +1,129 @@
+import os
+import unittest
+import numpy as np
+import arvet.util.image_utils as image_utils
+
+
+_test_dir = 'temp-deleteme'
+
+# An RGB image in array and PNG form, to test image reading
+_demo_image_rgb = np.array([
+    [[255 * r, 255 * g, 127.5 * (2 - r - g)] for r in np.arange(0, 1, 0.1)]
+    for g in np.arange(0, 1, 0.1)
+], dtype=np.uint8)
+_demo_image_rgb_binary = b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\n\x00\x00\x00\n\x08\x02\x00\x00\x00\x02PX" \
+                         b"\xea\x00\x00\x00\x94IDAT\x18\x19\x05\xc1QA\x02P\x00\x04\xc1}1\xeejP\xc3\x1c\xd4\xb0\x80" \
+                         b"\x05\x08`\x01\nP\x80\x00\x14\xa0\x00{\xff:s\xe0/X\x0c\x96\x05\x8b\xc1\xb2\xe0!\x9f`1X\x16" \
+                         b",\x06\xcb\x82\x87\xcb;X\x16,\x06\xcb\x82\xc5\xe0\xe1\xeb\x15,\x0b\x16\xc3\x8a\xc1b\xd8\xe1" \
+                         b"\xfa\x0c\x16\x83e\xc1b\xb0\x18v\xf8~\x04\x8ba\xc5`1X\x16<\xdc\xee\xc1\xb2`1X\x0c+\x06\x0f" \
+                         b"\xf7\xdf`Y\xb0\x18,\x0b\x16\xc3\x0e\xcf[\xb0\x18,\x0b\x16\x83\xc5\xb0\xc3\xfb'X\x0c\x96" \
+                         b"\x05\x8b\xc1\xb2\xe0?\xdf\xe5j\xaf\xb6\x15\xb3\xd2\x00\x00\x00\x00IEND\xaeB`\x82"
+
+# An depth image in array and PNG form with 16-bit depth, to test image reading
+_demo_image_depth = np.array([
+    [65535 * (1 - 0.5 * x - 0.5 * y) for x in np.arange(0, 1, 0.1)]
+    for y in np.arange(0, 1, 0.1)
+], dtype=np.uint16)
+_demo_image_depth_binary = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\n\x00\x00\x00\n\x10\x00\x00\x00\x00\xf8' \
+                           b'\xc9L"\x00\x00\x00kIDAT\x08\x1d\x05\xc1QU\x03Q\x10@\xb1\xf3,\xf5\x8e\xa5\xbaA\x005\x80' \
+                           b'\x81\n\xa8\x815\x80\x01\x0cd\xfe\x97\xe4\xdc\xf7&\x19m2:\x1e\x92\xcc&\x19\x9d\xbf\xa7' \
+                           b'\x8c6\x19i;\xbf\xdf2\x9b\x8c\xb4\x99s]\xdad\xa4M\xe6|\xeeMF\xdadt\xde\x0f\x19i\x93\x91' \
+                           b'\xce\xcfSF\x9b\x8c\xb4s^/i3\x926s\xbe.m2\xd2f\xf4\x0f\xa6Qu>\x91\xc9#\xb1' \
+                           b'\x00\x00\x00\x00IEND\xaeB`\x82'
+
+
+class TestImageUtils(unittest.TestCase):
+    rgb_image_path = None
+    depth_image_path = None
+
+    @classmethod
+    def setUpClass(cls):
+        # Create a test dir with the desired image files in it
+        if not os.path.isdir(_test_dir):
+            os.makedirs(_test_dir, exist_ok=True)
+        cls.rgb_image_path = os.path.join(_test_dir, 'demo_image_rgb.png')
+        with open(cls.rgb_image_path, "wb") as image_file:
+            image_file.write(_demo_image_rgb_binary)
+
+        cls.depth_image_path = os.path.join(_test_dir, 'demo_image_depth.png')
+        with open(cls.depth_image_path, "wb") as image_file:
+            image_file.write(_demo_image_depth_binary)
+
+    @classmethod
+    def tearDownClass(cls):
+        # Clean up the image files and test dir when we're done
+        if cls.rgb_image_path is not None:
+            os.remove(cls.rgb_image_path)
+        if cls.depth_image_path is not None:
+            os.remove(cls.depth_image_path)
+        os.removedirs(_test_dir)
+
+    def test_read_colour(self):
+        image_data = image_utils.read_colour(self.rgb_image_path)
+        self.assertNPEqual(_demo_image_rgb, image_data)
+
+    def test_read_depth(self):
+        image_data = image_utils.read_depth(self.depth_image_path)
+        self.assertNPEqual(_demo_image_depth, image_data)
+
+    def test_convert_to_grey_do_nothing(self):
+        image = np.array([[16 * y + x for x in range(0, 16)] for y in range(0, 16)], dtype=np.uint8)
+        result = image_utils.convert_to_grey(image)
+        self.assertNPEqual(image, result)
+
+    def test_convert_to_grey_monochrome(self):
+        monochrome = np.array([[[16 * y + x, 16 * y + x, 16 * y + x]
+                                for x in range(0, 16)]
+                               for y in range(0, 16)], dtype=np.uint8)
+        grey_image = image_utils.convert_to_grey(monochrome)
+        self.assertNPEqual(monochrome[:, :, 0], grey_image)
+
+    def test_convert_to_grey(self):
+        rgb_image = np.array([
+            [[255 * r, 255 * g, 127.5 * (2 - r - g)] for r in np.arange(0, 1, 0.01)]
+            for g in np.arange(0, 1, 0.01)
+        ], dtype=np.uint8)
+        result = image_utils.convert_to_grey(rgb_image)
+        self.assertEqual(2, len(result.shape))
+        self.assertEqual(rgb_image.shape[0:2], result.shape)
+        self.assertNotNPEqual(rgb_image[:, :, 0], result)
+        self.assertNotNPEqual(rgb_image[:, :, 1], result)
+        self.assertNotNPEqual(rgb_image[:, :, 2], result)
+
+    def test_get_bounding_box_logical(self):
+        logical_image = np.array([[False for _ in range(100)] for _ in range(100)])
+        left = 12
+        top = 32
+        right = 73
+        bottom = 56
+        logical_image[top:bottom, left:right] = True
+        self.assertEqual((left, top, right, bottom), image_utils.get_bounding_box(logical_image))
+
+    def test_get_bounding_box_integer(self):
+        logical_image = np.array([[0 for _ in range(100)] for _ in range(100)], dtype=np.uint8)
+        left = 12
+        top = 32
+        right = 73
+        bottom = 56
+        logical_image[top:bottom, left:right] = 255
+        self.assertEqual((left, top, right, bottom), image_utils.get_bounding_box(logical_image))
+
+    def test_get_bounding_box_float(self):
+        logical_image = np.array([[0.0 for _ in range(100)] for _ in range(100)], dtype=np.float32)
+        left = 12
+        top = 32
+        right = 73
+        bottom = 56
+        logical_image[top:bottom, left:right] = 1.0
+        self.assertEqual((left, top, right, bottom), image_utils.get_bounding_box(logical_image))
+
+    def test_show_image_float(self):
+        image_data = np.array([[0.0 for _ in range(100)] for _ in range(100)], dtype=np.float32)
+        image_data[12:56, 34:75] = 1.0
+        image_utils.show_image(image_data, 'test window')
+
+    def assertNPEqual(self, arr1, arr2):
+        self.assertTrue(np.array_equal(arr1, arr2), "Arrays {0} and {1} are not equal".format(str(arr1), str(arr2)))
+
+    def assertNotNPEqual(self, arr1, arr2):
+        self.assertFalse(np.array_equal(arr1, arr2), "Arrays {0} and {1} are the same".format(str(arr1), str(arr2)))
