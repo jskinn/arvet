ddde1ba2396b203fddd3ce74147cc7d70db68c57
John Skinner <jskinn7@gmail.com>
2018-10-17 09:04:11 +1000

    Update reset_permissions.sh

--- reset_permissions.sh
+++ reset_permissions.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # Reset the file permissions for all the relevant files
 chmod +x reset_permissions.sh
-chmod -x README.md .gitignore requirements.txt LICENSE
+chmod -R o-w .
+chmod -R g-w .
+chmod -x README.rst .gitignore requirements.txt LICENSE
 find . -name "*.py" -exec chmod -x {} \;
-chmod +x add_initial_entities.py
-chmod +x scheduler.py
-chmod +x run_task.py
-chmod +x plot_results.py
-chmod +x verify_bounding_boxes_manually.py
-chmod +x verify_database.py
-chmod +x invalidate_data.py
+chmod +x arvet/scheduler.py
+chmod +x arvet/plot_results.py
+chmod +x arvet/verify_bounding_boxes_manually.py
+chmod +x arvet/verify_database.py
+chmod +x arvet/invalidate_data.py
