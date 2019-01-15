a7d37753db1bd11d195551875a88b73183a9e2f9
John Skinner <jskinn7@gmail.com>
2019-01-15 12:00:49 +1000

    Update reset permissions.

--- reset_permissions.sh
+++ reset_permissions.sh
@@ -3,10 +3,12 @@
 chmod +x reset_permissions.sh
 chmod -R o-w .
 chmod -R g-w .
-chmod -x README.rst .gitignore requirements.txt LICENSE
+chmod -x arvet/simulation/tests/test-depth-left.npy arvet/simulation/tests/test-depth-right.npy
+chmod -x README.rst .gitignore requirements.txt LICENSE environment.yml
 find . -name "*.py" -exec chmod -x {} \;
 chmod +x arvet/scheduler.py
 chmod +x arvet/plot_results.py
 chmod +x arvet/verify_bounding_boxes_manually.py
 chmod +x arvet/verify_database.py
 chmod +x arvet/invalidate_data.py
+chmod +x arvet/batch_analysis/scripts/run_task.py
