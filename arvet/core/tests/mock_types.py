cea5dafddd5da64d5f45ac006303c5ed0d9116d8
John Skinner <jskinn7@gmail.com>
2019-01-16 19:17:07 +1000

    Move image creation helpers to mock_types.

--- arvet/core/tests/mock_types.py
+++ arvet/core/tests/mock_types.py
@@ -3,6 +3,11 @@
 Minimal types for many of the core abstract base classes.
 This allows other tests to use instances of these types.
 """
+import numpy as np
+import arvet.util.transform as tf
+import arvet.util.dict_utils as du
+import arvet.metadata.image_metadata as imeta
+import arvet.core.image as im
 import arvet.core.system
 import arvet.core.image_source
 import arvet.core.trial_result
@@ -66,3 +71,81 @@ class MockTrialComparisonMetric(arvet.core.trial_comparison.TrialComparisonMetri
             reference_trial_results=list(reference_trial_results),
             success=True
         )
+
+
+def make_metadata(index=1, **kwargs):
+    kwargs = du.defaults(kwargs, {
+        'img_hash': b'\xf1\x9a\xe2|' + np.random.randint(0, 0xFFFFFFFF).to_bytes(4, 'big'),
+        'source_type': imeta.ImageSourceType.SYNTHETIC,
+        'camera_pose': tf.Transform(location=(1 + 100 * index, 2 + np.random.uniform(-1, 1), 3),
+                                    rotation=(4, 5, 6, 7 + np.random.uniform(-4, 4))),
+        'intrinsics': cam_intr.CameraIntrinsics(800, 600, 550.2, 750.2, 400, 300),
+        'environment_type': imeta.EnvironmentType.INDOOR_CLOSE,
+        'light_level': imeta.LightingLevel.WELL_LIT, 'time_of_day': imeta.TimeOfDay.DAY,
+        'lens_focal_distance': 5,
+        'aperture': 22,
+        'simulation_world': 'TestSimulationWorld',
+        'lighting_model': imeta.LightingModel.LIT,
+        'texture_mipmap_bias': 1,
+        'normal_maps_enabled': 2,
+        'roughness_enabled': True,
+        'geometry_decimation': 0.8,
+        'procedural_generation_seed': 16234,
+        'labelled_objects': (
+            imeta.LabelledObject(
+                class_names=('car',), x=12, y=144, width=67, height=43,
+                relative_pose=tf.Transform((12, 3, 4), (0.5, 0.1, 1, 1.7)),
+                instance_name='Car-002'
+            ),
+            imeta.LabelledObject(
+                class_names=('cat',), x=125, y=244, width=117, height=67,
+                relative_pose=tf.Transform((378, -1890, 38), (0.3, 1.12, 1.1, 0.2)),
+                instance_name='cat-090'
+            )
+        )
+    })
+    return imeta.ImageMetadata(**kwargs)
+
+
+def make_image(index=1, **kwargs):
+    kwargs = du.defaults(kwargs, {
+        'pixels': np.random.uniform(0, 255, (32, 32, 3)),
+        'metadata': make_metadata(index),
+        'additional_metadata': {
+            'Source': 'Generated',
+            'Resolution': {'width': 1280, 'height': 720},
+            'Material Properties': {
+                'BaseMipMapBias': 0,
+                'RoughnessQuality': True
+            }
+        },
+        'depth': np.random.uniform(0, 1, (32, 32)),
+        'normals': np.random.uniform(0, 1, (32, 32, 3))
+    })
+    return im.Image(**kwargs)
+
+
+def make_stereo_image(index=1, **kwargs):
+    kwargs = du.defaults(kwargs, {
+        'pixels': np.random.uniform(0, 255, (32, 32, 3)),
+        'right_pixels': np.random.uniform(0, 255, (32, 32, 3)),
+        'metadata': make_metadata(index),
+        'right_metadata': make_metadata(
+            index,
+            camera_pose=tf.Transform(location=(1 + 100*index, 2 + np.random.uniform(-1, 1), 4),
+                                     rotation=(4, 5, 6, 7 + np.random.uniform(-4, 4))),
+        ),
+        'additional_metadata': {
+            'Source': 'Generated',
+            'Resolution': {'width': 1280, 'height': 720},
+            'Material Properties': {
+                'BaseMipMapBias': 0,
+                'RoughnessQuality': True
+            }
+        },
+        'depth': np.random.uniform(0, 1, (32, 32)),
+        'right_depth': np.random.uniform(0, 1, (32, 32)),
+        'normals': np.random.uniform(0, 1, (32, 32, 3)),
+        'right_normals': np.random.uniform(0, 1, (32, 32, 3))
+    })
+    return im.StereoImage(**kwargs)
--- arvet/core/tests/test_image_collection.py
+++ arvet/core/tests/test_image_collection.py
@@ -5,13 +5,12 @@ from operator import itemgetter
 import numpy as np
 import arvet.database.tests.database_connection as dbconn
 import arvet.database.image_manager as im_manager
-import arvet.util.dict_utils as du
 import arvet.util.transform as tf
-import arvet.metadata.camera_intrinsics as cam_intr
 import arvet.metadata.image_metadata as imeta
 import arvet.core.image as im
 import arvet.core.image_collection as ic
 from arvet.core.sequence_type import ImageSequenceType
+from arvet.core.tests.mock_types import make_metadata, make_image, make_stereo_image
 
 
 class TestImageCollectionDatabase(unittest.TestCase):
@@ -380,81 +379,3 @@ class TestImageCollection(unittest.TestCase):
     #     for image_id in subject._images.values():
     #         self.assertIn(mock.call('{0}/image_cache/{1}.pickle'.format(db_client.temp_folder, image_id), 'wb'),
     #                       mock_open.call_args_list)
-
-
-def make_metadata(index=1, **kwargs):
-    kwargs = du.defaults(kwargs, {
-        'img_hash': b'\xf1\x9a\xe2|' + np.random.randint(0, 0xFFFFFFFF).to_bytes(4, 'big'),
-        'source_type': imeta.ImageSourceType.SYNTHETIC,
-        'camera_pose': tf.Transform(location=(1 + 100 * index, 2 + np.random.uniform(-1, 1), 3),
-                                    rotation=(4, 5, 6, 7 + np.random.uniform(-4, 4))),
-        'intrinsics': cam_intr.CameraIntrinsics(800, 600, 550.2, 750.2, 400, 300),
-        'environment_type': imeta.EnvironmentType.INDOOR_CLOSE,
-        'light_level': imeta.LightingLevel.WELL_LIT, 'time_of_day': imeta.TimeOfDay.DAY,
-        'lens_focal_distance': 5,
-        'aperture': 22,
-        'simulation_world': 'TestSimulationWorld',
-        'lighting_model': imeta.LightingModel.LIT,
-        'texture_mipmap_bias': 1,
-        'normal_maps_enabled': 2,
-        'roughness_enabled': True,
-        'geometry_decimation': 0.8,
-        'procedural_generation_seed': 16234,
-        'labelled_objects': (
-            imeta.LabelledObject(
-                class_names=('car',), x=12, y=144, width=67, height=43,
-                relative_pose=tf.Transform((12, 3, 4), (0.5, 0.1, 1, 1.7)),
-                instance_name='Car-002'
-            ),
-            imeta.LabelledObject(
-                class_names=('cat',), x=125, y=244, width=117, height=67,
-                relative_pose=tf.Transform((378, -1890, 38), (0.3, 1.12, 1.1, 0.2)),
-                instance_name='cat-090'
-            )
-        )
-    })
-    return imeta.ImageMetadata(**kwargs)
-
-
-def make_image(index=1, **kwargs):
-    kwargs = du.defaults(kwargs, {
-        'pixels': np.random.uniform(0, 255, (32, 32, 3)),
-        'metadata': make_metadata(index),
-        'additional_metadata': {
-            'Source': 'Generated',
-            'Resolution': {'width': 1280, 'height': 720},
-            'Material Properties': {
-                'BaseMipMapBias': 0,
-                'RoughnessQuality': True
-            }
-        },
-        'depth': np.random.uniform(0, 1, (32, 32)),
-        'normals': np.random.uniform(0, 1, (32, 32, 3))
-    })
-    return im.Image(**kwargs)
-
-
-def make_stereo_image(index=1, **kwargs):
-    kwargs = du.defaults(kwargs, {
-        'pixels': np.random.uniform(0, 255, (32, 32, 3)),
-        'right_pixels': np.random.uniform(0, 255, (32, 32, 3)),
-        'metadata': make_metadata(index),
-        'right_metadata': make_metadata(
-            index,
-            camera_pose=tf.Transform(location=(1 + 100*index, 2 + np.random.uniform(-1, 1), 4),
-                                     rotation=(4, 5, 6, 7 + np.random.uniform(-4, 4))),
-        ),
-        'additional_metadata': {
-            'Source': 'Generated',
-            'Resolution': {'width': 1280, 'height': 720},
-            'Material Properties': {
-                'BaseMipMapBias': 0,
-                'RoughnessQuality': True
-            }
-        },
-        'depth': np.random.uniform(0, 1, (32, 32)),
-        'right_depth': np.random.uniform(0, 1, (32, 32)),
-        'normals': np.random.uniform(0, 1, (32, 32, 3)),
-        'right_normals': np.random.uniform(0, 1, (32, 32, 3))
-    })
-    return im.StereoImage(**kwargs)
