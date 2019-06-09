01196ddd78f0396aac8b14746be7f11607c99d0f
John Skinner <jskinn7@gmail.com>
2019-06-09 22:35:02 +1000

    Solve the problem of deleting from the image manager when objects with image fields are deleted. Also better tests for image fields. However, models with image fields must use a custom manager.

--- arvet/database/image_field.py
+++ arvet/database/image_field.py
@@ -1,5 +1,7 @@
 import pymodm
 from pymodm import validators
+from pymodm.queryset import QuerySet
+from pymodm.manager import Manager
 import numpy as np
 import arvet.database.image_manager
 
@@ -11,9 +13,10 @@ class ImageField(pymodm.fields.MongoBaseField):
     (see arvet.database.image_manager)
     """
 
-    def __init__(self, verbose_name=None, mongo_name=None, **kwargs):
+    def __init__(self, group='', verbose_name=None, mongo_name=None, **kwargs):
         """
 
+        :param group: A subgroup to store the image under
         :param verbose_name: The human-readable name of this field
         :param mongo_name: The name of this field in mongodb
         :param kwargs: Additional kwargs passed to MongoBaseField
@@ -23,11 +26,12 @@ class ImageField(pymodm.fields.MongoBaseField):
             mongo_name=mongo_name,
             **kwargs
         )
+        self._group = group
         self.validators.append(validators.validator_for_type(np.ndarray))
 
     def __set__(self, inst, value):
         if isinstance(value, np.ndarray):
-            # Numpy arrays are python values, set as such. Otherwise, they will set as
+            # Numpy arrays are python values, set as such. Otherwise, they will set as mongo values
             inst._data.set_python_value(self.attname, value)
         else:
             super(ImageField, self).__set__(inst, value)
@@ -53,6 +57,66 @@ class ImageField(pymodm.fields.MongoBaseField):
     def to_mongo(self, value):
         if isinstance(value, np.ndarray):
             with arvet.database.image_manager.get() as image_manager:
-                path = image_manager.store_image(value)
+                path = image_manager.store_image(value, group=self._group)
             return path
         return value
+
+
+class ImageQuerySet(QuerySet):
+    """
+    A custom query set for models that use image fields.
+    Must use this to suport deleting images from the image manager
+    when the corresponding model is deleted.
+    """
+
+    def delete(self):
+        """
+        Delete all the models in the query set.
+        Models delegate to this
+        :return:
+        """
+        # Search the model, and all embedded models for ImageFields
+        image_paths = []
+        to_search = [
+            (self._model, [doc for doc in self.values()])
+        ]
+        while len(to_search) > 0:
+            model, docs = to_search.pop()
+
+            # Find all the Image fields on this model
+            fields = model._mongometa.get_fields()
+            image_fields = [
+                field.attname
+                for field in fields
+                if isinstance(field, ImageField)
+            ]
+
+            # Get the values of those fields from the documents
+            image_paths.extend(doc[name] for name in image_fields for doc in docs)
+
+            # Look in embedded documents as well
+            to_search.extend(
+                (field.related_model, [doc[field.attname] for doc in docs])
+                for field in fields
+                if isinstance(field, pymodm.fields.EmbeddedDocumentField)
+            )
+            to_search.extend(
+                (field.related_model, [inner_doc for doc in docs for inner_doc in doc[field.attname]])
+                for field in fields
+                if isinstance(field, pymodm.fields.EmbeddedDocumentListField)
+            )
+
+        # Delete all the images at those paths
+        if len(image_paths) > 0:
+            with arvet.database.image_manager.get() as image_manager:
+                for path in image_paths:
+                    image_manager.remove_image(path)
+
+        # Proceed with the rest of the delete
+        super(ImageQuerySet, self).delete()
+
+
+# Custom manager using the ImageQueryset by default.
+# Assign this as the manager for models with image fields
+ImageManager = Manager.from_queryset(ImageQuerySet)
+
--- arvet/database/tests/test_image_field.py
+++ arvet/database/tests/test_image_field.py
@@ -3,12 +3,40 @@ import pymodm
 import numpy as np
 from arvet.util.test_helpers import ExtendedTestCase
 import arvet.database.tests.database_connection as dbconn
-from arvet.database.image_field import ImageField
+from arvet.database.image_field import ImageField, ImageManager
 import arvet.database.image_manager as im_manager
 
 
-class TestImageFieldMongoModel(pymodm.MongoModel):
+class TestImageFieldModel(pymodm.MongoModel):
     image = ImageField()
+    objects = ImageManager()
+
+
+class TestImageFieldEmbeddedModel(pymodm.EmbeddedMongoModel):
+    image = ImageField()
+
+
+class TestImageInEmbeddedDocumentModel(pymodm.MongoModel):
+    inner = pymodm.fields.EmbeddedDocumentField(TestImageFieldEmbeddedModel)
+    objects = ImageManager()
+
+
+class TestImageInEmbeddedDocumentListModel(pymodm.MongoModel):
+    inner_list = pymodm.fields.EmbeddedDocumentListField(TestImageFieldEmbeddedModel)
+    objects = ImageManager()
+
+
+class TestNestedEmbeddedDoc(pymodm.EmbeddedMongoModel):
+    inner = pymodm.fields.EmbeddedDocumentField(TestImageFieldEmbeddedModel)
+
+
+class TestComplexImageModel(pymodm.MongoModel):
+    image = ImageField()
+    inner = pymodm.fields.EmbeddedDocumentField(TestImageFieldEmbeddedModel)
+    nested_inner = pymodm.fields.EmbeddedDocumentField(TestNestedEmbeddedDoc)
+    inner_list = pymodm.fields.EmbeddedDocumentListField(TestImageFieldEmbeddedModel)
+    nested_inner_list = pymodm.fields.EmbeddedDocumentListField(TestNestedEmbeddedDoc)
+    objects = ImageManager()
 
 
 class TestImageField(ExtendedTestCase):
@@ -22,7 +50,10 @@ class TestImageField(ExtendedTestCase):
         # Clean up after ourselves by dropping the collection for this model and removing the image file
         if os.path.isfile(dbconn.image_file):
             os.remove(dbconn.image_file)
-        TestImageFieldMongoModel._mongometa.collection.drop()
+        TestImageFieldModel._mongometa.collection.drop()
+        TestImageInEmbeddedDocumentModel._mongometa.collection.drop()
+        TestImageInEmbeddedDocumentListModel._mongometa.collection.drop()
+        TestComplexImageModel._mongometa.collection.drop()
 
     def test_image_field_stores_and_loads(self):
         image = np.random.randint(0, 255, size=(64, 64, 3), dtype=np.uint8)
@@ -30,15 +61,35 @@ class TestImageField(ExtendedTestCase):
         im_manager.set_image_manager(image_manager)
 
         # Save the image
-        model = TestImageFieldMongoModel()
+        model = TestImageFieldModel()
         model.image = image
         model.save()
 
         # Load all the entities
-        all_entities = list(TestImageFieldMongoModel.objects.all())
-        self.assertGreaterEqual(len(all_entities), 1)
+        all_entities = list(TestImageFieldModel.objects.all())
+        self.assertEqual(len(all_entities), 1)
         self.assertNPEqual(all_entities[0].image, image)
         all_entities[0].delete()
+
+    def test_group_modifies_storage_path(self):
+        image = np.random.randint(0, 255, size=(64, 64, 3), dtype=np.uint8)
+        image_manager = im_manager.DefaultImageManager(dbconn.image_file)
+        im_manager.set_image_manager(image_manager)
+
+        # Define a custom model class
+        class GroupedImageModel(pymodm.MongoModel):
+            image = ImageField(group='mygroup')
+
+        model = GroupedImageModel(image=image)
+        model.save()
+
+        # Get the unserialised documents
+        try:
+            documents = list(GroupedImageModel.objects.all().values())
+            self.assertEqual(len(documents), 1)
+            self.assertTrue(documents[0]['image'].startswith('mygroup/'))
+        finally:
+            GroupedImageModel._mongometa.collection.drop()
 
     def test_delete_removes(self):
         image = np.random.randint(0, 255, size=(64, 64, 3), dtype=np.uint8)
@@ -47,7 +98,7 @@ class TestImageField(ExtendedTestCase):
         path = image_manager.find_path_for_image(image)
 
         # Save the image
-        model = TestImageFieldMongoModel()
+        model = TestImageFieldModel()
         model.image = image
         model.save()
         self.assertTrue(image_manager.is_valid_path(path))
@@ -55,3 +106,139 @@ class TestImageField(ExtendedTestCase):
         # Delete the model
         model.delete()
         self.assertFalse(image_manager.is_valid_path(path))
+
+    def test_delete_removes_from_embedded_model(self):
+        image = np.random.randint(0, 255, size=(64, 64, 3), dtype=np.uint8)
+        image_manager = im_manager.DefaultImageManager(dbconn.image_file)
+        im_manager.set_image_manager(image_manager)
+        path = image_manager.find_path_for_image(image)
+
+        # Save the image
+        model = TestImageInEmbeddedDocumentModel()
+        model.inner = TestImageFieldEmbeddedModel()
+        model.inner.image = image
+        model.save()
+        self.assertTrue(image_manager.is_valid_path(path))
+
+        # Delete the model
+        model.delete()
+        self.assertFalse(image_manager.is_valid_path(path))
+
+    def test_delete_removes_from_embedded_model_list(self):
+        image_manager = im_manager.DefaultImageManager(dbconn.image_file)
+        im_manager.set_image_manager(image_manager)
+
+        image1 = np.random.randint(0, 255, size=(64, 64, 3), dtype=np.uint8)
+        image2 = np.random.randint(0, 255, size=(64, 64, 3), dtype=np.uint8)
+        path1 = image_manager.find_path_for_image(image1)
+        path2 = image_manager.find_path_for_image(image2)
+
+        # Build the model
+        inner1 = TestImageFieldEmbeddedModel(image=image1)
+        inner2 = TestImageFieldEmbeddedModel(image=image2)
+        model = TestImageInEmbeddedDocumentListModel(inner_list=[inner1, inner2])
+        model.save()
+        self.assertTrue(image_manager.is_valid_path(path1))
+        self.assertTrue(image_manager.is_valid_path(path2))
+
+        # Delete the model
+        model.delete()
+        self.assertFalse(image_manager.is_valid_path(path1))
+        self.assertFalse(image_manager.is_valid_path(path2))
+
+    def test_delete_removes_from_complex_model(self):
+        image_manager = im_manager.DefaultImageManager(dbconn.image_file)
+        im_manager.set_image_manager(image_manager)
+
+        images = [
+            np.random.randint(0, 255, size=(64, 64, 3), dtype=np.uint8)
+            for _ in range(7)
+        ]
+        paths = [
+            image_manager.find_path_for_image(image)
+            for image in images
+        ]
+
+        # Build the model
+        inners = [
+            TestImageFieldEmbeddedModel(image=images[idx])
+            for idx in range(6)
+        ]
+
+        nested1 = TestNestedEmbeddedDoc(inner=inners[0])
+        nested2 = TestNestedEmbeddedDoc(inner=inners[1])
+        nested3 = TestNestedEmbeddedDoc(inner=inners[2])
+
+        model = TestComplexImageModel(
+            image=images[6],
+            inner=inners[3],
+            nested_inner=nested1,
+            inner_list=[inners[4], inners[5]],
+            nested_inner_list=[nested2, nested3]
+        )
+        model.save()
+        for path in paths:
+            self.assertTrue(image_manager.is_valid_path(path))
+
+        # Delete the model
+        model.delete()
+        for path in paths:
+            self.assertFalse(image_manager.is_valid_path(path))
+
+    def test_delete_removes_from_lots_of_complex_models(self):
+        image_manager = im_manager.DefaultImageManager(dbconn.image_file)
+        im_manager.set_image_manager(image_manager)
+
+        ids_to_delete = []
+        kept_paths = []
+        deleted_paths = []
+        for idx in range(10):
+            # Make the images
+            images = [
+                np.random.randint(0, 255, size=(64, 64, 3), dtype=np.uint8)
+                for _ in range(7)
+            ]
+            if idx % 2 == 0:
+                deleted_paths.extend(
+                    image_manager.find_path_for_image(image)
+                    for image in images
+                )
+            else:
+                kept_paths.extend(
+                    image_manager.find_path_for_image(image)
+                    for image in images
+                )
+
+            # Build the inner models
+            inners = [
+                TestImageFieldEmbeddedModel(image=images[idx])
+                for idx in range(6)
+            ]
+
+            nested1 = TestNestedEmbeddedDoc(inner=inners[0])
+            nested2 = TestNestedEmbeddedDoc(inner=inners[1])
+            nested3 = TestNestedEmbeddedDoc(inner=inners[2])
+
+            # Make a complex model
+            model = TestComplexImageModel(
+                image=images[6],
+                inner=inners[3],
+                nested_inner=nested1,
+                inner_list=[inners[4], inners[5]],
+                nested_inner_list=[nested2, nested3]
+            )
+            model.save()
+            if idx % 2 == 0:
+                ids_to_delete.append(model.pk)
+
+        for path in kept_paths:
+            self.assertTrue(image_manager.is_valid_path(path))
+        for path in deleted_paths:
+            self.assertTrue(image_manager.is_valid_path(path))
+
+        # Delete some of the models
+        TestComplexImageModel.objects.raw({'_id': {'$in': ids_to_delete}}).delete()
+        for path in kept_paths:
+            self.assertTrue(image_manager.is_valid_path(path))
+        for path in deleted_paths:
+            self.assertFalse(image_manager.is_valid_path(path))
