4a1509e3216b2e1dff80775b7eb72d1f3ea0dd53
John Skinner <jskinn7@gmail.com>
2019-06-09 15:06:19 +1000

    Significantly expand the unit tests for the image manager, and tweaks to its design. Now supports storing images in groups, so simplify hash collision avoidance.

--- arvet/database/image_manager.py
+++ arvet/database/image_manager.py
@@ -36,16 +36,22 @@ class ImageManager(metaclass=abc.ABCMeta):
         pass
 
     @abc.abstractmethod
-    def store_image(self, data: np.ndarray) -> str:
+    def store_image(self, data: np.ndarray, group: str = '') -> str:
         """
         Store an image.
-        :param data:
+        :param data: The image data to store
+        :param group:
         :return:
         """
         pass
 
     @abc.abstractmethod
     def remove_image(self, path: str) -> bool:
+        """
+        Remove an image from the store
+        :param path:
+        :return:
+        """
         pass
 
 
@@ -57,7 +63,7 @@ class DefaultImageManager(ImageManager):
 
     def __init__(self, file_path, group_name=''):
         self._file_path = file_path
-        self._prefix = group_name.rstrip('/')
+        self._prefix = group_name.strip('/')
         self._contexts = 0
         self._storage = None
 
@@ -77,20 +83,23 @@ class DefaultImageManager(ImageManager):
 
     def get_image(self, path: str) -> typing.Union[np.ndarray, None]:
         with self:
-            return np.array(self._storage.get(path, default=None))
+            data = self._storage.get(path, default=None)
+            if data is not None:
+                return np.array(data)
+        return None
 
     def is_valid_path(self, path: str) -> bool:
         with self:
             return path in self._storage
 
-    def store_image(self, data: np.ndarray) -> str:
+    def store_image(self, data: np.ndarray, group: str = '') -> str:
         # Use a context to open our file if it is not already open
         with self:
             # Try different seeds until we find one that doesn't collide, or we find that this image is already stored
             # Since the iteration is in the same order every time, and we check equality, will always resolve collisions
             # if it is possible to do so.
             for seed in range(2 ** 32):
-                path = self.find_path_for_image(data, seed)
+                path = self.find_path_for_image(data, group, seed)
                 if path not in self._storage:
                     # Hash value is available, store the data there
                     self._storage[path] = data
@@ -107,14 +116,17 @@ class DefaultImageManager(ImageManager):
                 return True
         return False
 
-    def find_path_for_image(self, data, seed=0):
+    def find_path_for_image(self, data, group='', seed=0):
         """
         Find a valid path to store this image, based on a hash of the
         :param data:
+        :param group:
         :param seed: A seed passed to the hash, for collision handling.
         :return:
         """
-        return self._prefix + xxhash.xxh64(data, seed=seed).hexdigest()
+        group = group.strip('/')
+        digest = xxhash.xxh64(data, seed=seed).hexdigest()
+        return '/'.join(part for part in (self._prefix, group, digest) if len(part) > 0)
 
 
 # ----- Global Singleton -----
@@ -151,5 +163,5 @@ def get() -> ImageManager:
     :return:
     """
     if __image_manager is None:
-        raise RuntimeWarning("No image manager configured, did you forget to call 'configure'?")
+        raise RuntimeError("No image manager configured, did you forget to call 'configure'?")
     return __image_manager
--- arvet/database/tests/test_image_manager.py
+++ arvet/database/tests/test_image_manager.py
@@ -1,6 +1,7 @@
 import os.path
-import unittest.mock
+import unittest.mock as mock
 import numpy as np
+import xxhash
 import h5py
 import arvet.database.image_manager as im_manager
 from arvet.util.test_helpers import ExtendedTestCase
@@ -20,15 +21,98 @@ class TestDefaultImageManager(ExtendedTestCase):
         if os.path.isfile(cls._tempfile):
             os.remove(cls._tempfile)
 
-    def make_instance(self):
-        return im_manager.DefaultImageManager(self._tempfile)
+    def test_get_image_for_missing_path_returns_none(self):
+        subject = im_manager.DefaultImageManager(self._tempfile)
+        self.assertIsNone(subject.get_image('notapath'))
 
     def test_store_and_get_image(self):
-        subject = self.make_instance()
+        subject = im_manager.DefaultImageManager(self._tempfile)
         image = np.random.randint(0, 255, dtype=np.uint8, size=(64, 64, 3))
         path = subject.store_image(image)
         output = subject.get_image(path)
         self.assertNPEqual(image, output)
+
+    def test_storing_the_same_image_repeatedly_gives_the_same_path(self):
+        subject = im_manager.DefaultImageManager(self._tempfile)
+        image = np.random.randint(0, 255, dtype=np.uint8, size=(64, 64, 3))
+        path1 = subject.store_image(image)
+        path2 = subject.store_image(image)
+        self.assertEqual(path1, path2)
+
+    def test_is_valid_path_returns_true_only_for_valid_paths(self):
+        subject = im_manager.DefaultImageManager(self._tempfile)
+        image = np.random.randint(0, 255, dtype=np.uint8, size=(64, 64, 3))
+        path = subject.store_image(image)
+
+        self.assertTrue(subject.is_valid_path(path))
+        self.assertFalse(subject.is_valid_path('notapath'))
+
+    def test_remove_image_deletes_the_image(self):
+        subject = im_manager.DefaultImageManager(self._tempfile)
+        image1 = np.random.randint(0, 255, dtype=np.uint8, size=(64, 64, 3))
+        image2 = np.random.randint(0, 255, dtype=np.uint8, size=(64, 64, 3))
+        path1 = subject.store_image(image1)
+        path2 = subject.store_image(image2)
+
+        self.assertTrue(subject.is_valid_path(path1))
+        self.assertTrue(subject.is_valid_path(path2))
+
+        subject.remove_image(path1)
+
+        self.assertFalse(subject.is_valid_path(path1))
+        self.assertTrue(subject.is_valid_path(path2))
+        self.assertIsNone(subject.get_image(path1))
+        self.assertNPEqual(image2, subject.get_image(path2))
+
+    @mock.patch('arvet.database.image_manager.xxhash', autospec=True)
+    def test_can_use_groups_to_avoid_hash_collisions(self, mock_xxhash):
+        subject = im_manager.DefaultImageManager(self._tempfile)
+        image1 = np.random.randint(0, 255, dtype=np.uint8, size=(64, 64, 3))
+        image2 = np.random.randint(0, 255, dtype=np.uint8, size=(64, 64, 3))
+
+        # Patch the hash function to force a hash collision
+        digest = 'FFAA12'
+        mock_hash64 = mock.MagicMock()
+        mock_hash64.hexdigest.return_value = digest
+        mock_xxhash.xxh64.return_value = mock_hash64
+
+        group1 = 'mygroup1'
+        group2 = '/mygroup2/'
+        path1 = subject.store_image(image1, group1)
+        path2 = subject.store_image(image2, group2)
+        self.assertEqual(group1 + '/' + digest, path1)
+        self.assertEqual('mygroup2/' + digest, path2)
+
+        output = subject.get_image(path1)
+        self.assertNPEqual(image1, output)
+
+        output = subject.get_image(path2)
+        self.assertNPEqual(image2, output)
+
+    @mock.patch('arvet.database.image_manager.xxhash', autospec=True)
+    def test_store_image_changes_seed_to_avoid_collisions(self, mock_xxhash):
+        subject = im_manager.DefaultImageManager(self._tempfile)
+        image1 = np.random.randint(0, 255, dtype=np.uint8, size=(64, 64, 3))
+        image2 = np.random.randint(0, 255, dtype=np.uint8, size=(64, 64, 3))
+
+        # Patch the hash function to force a hash collision for the same seed
+        def mock_hash_func(_, seed=0):
+            mock_hash64 = mock.MagicMock()
+            mock_hash64.hexdigest.return_value = 'FFAA12' + str(seed)
+            return mock_hash64
+
+        mock_xxhash.xxh64.side_effect = mock_hash_func
+
+        path1 = subject.store_image(image1)
+        path2 = subject.store_image(image2)
+        self.assertEqual('FFAA120', path1)
+        self.assertEqual('FFAA121', path2)
+
+        output = subject.get_image(path1)
+        self.assertNPEqual(image1, output)
+
+        output = subject.get_image(path2)
+        self.assertNPEqual(image2, output)
 
     def test_context_opens_and_closes_file(self):
         original_cls = h5py.File
@@ -48,15 +132,41 @@ class TestDefaultImageManager(ExtendedTestCase):
             file.close = close_patch(file.close)
             return file
 
-        subject = self.make_instance()
-        with unittest.mock.patch('arvet.database.image_manager.h5py.File', file_patch):
+        subject = im_manager.DefaultImageManager(self._tempfile)
+        with mock.patch('arvet.database.image_manager.h5py.File', file_patch):
             self.assertFalse(opened)
             with subject:
                 self.assertTrue(opened)
             self.assertFalse(opened)
 
+    def test_context_closes_even_if_exception_raised(self):
+        original_cls = h5py.File
+        closed = False
+
+        def close_patch(original):
+            def patch(*args, **kwargs):
+                nonlocal closed
+                closed = True
+                original(*args, **kwargs)
+            return patch
+
+        def file_patch(*args, **kwargs):
+            file = original_cls(*args, **kwargs)
+            file.close = close_patch(file.close)
+            return file
+
+        subject = im_manager.DefaultImageManager(self._tempfile)
+        with mock.patch('arvet.database.image_manager.h5py.File', file_patch):
+            self.assertFalse(closed)
+            try:
+                with subject:
+                    raise ValueError()
+            except ValueError:
+                pass
+            self.assertTrue(closed)
+
     def test_handles_nested_contexts(self):
-        subject = self.make_instance()
+        subject = im_manager.DefaultImageManager(self._tempfile)
         image = np.random.randint(0, 255, dtype=np.uint8, size=(64, 64, 3))
         path = subject.store_image(image)
 
@@ -66,3 +176,40 @@ class TestDefaultImageManager(ExtendedTestCase):
                 self.assertNPEqual(image, output)
             output = subject.get_image(path)
             self.assertNPEqual(image, output)
+
+    def test_find_path_for_image_no_groups(self):
+        subject = im_manager.DefaultImageManager(self._tempfile)
+        image = np.random.randint(0, 255, dtype=np.uint8, size=(64, 64, 3))
+        digest = xxhash.xxh64(image, seed=0).hexdigest()
+        self.assertEqual(digest, subject.find_path_for_image(image))
+
+    def test_find_path_for_image_with_manager_group(self):
+        subject = im_manager.DefaultImageManager(self._tempfile, group_name='test')
+        image = np.random.randint(0, 255, dtype=np.uint8, size=(64, 64, 3))
+        digest = xxhash.xxh64(image, seed=0).hexdigest()
+        self.assertEqual('test/' + digest, subject.find_path_for_image(image))
+
+    def test_find_path_strips_preceding_slashes(self):
+        subject = im_manager.DefaultImageManager(self._tempfile, group_name='/test/foo/')
+        image = np.random.randint(0, 255, dtype=np.uint8, size=(64, 64, 3))
+        digest = xxhash.xxh64(image, seed=0).hexdigest()
+        self.assertEqual('test/foo/' + digest, subject.find_path_for_image(image))
+
+    def test_find_path_for_image_with_image_group(self):
+        subject = im_manager.DefaultImageManager(self._tempfile)
+        image = np.random.randint(0, 255, dtype=np.uint8, size=(64, 64, 3))
+        digest = xxhash.xxh64(image, seed=0).hexdigest()
+        self.assertEqual('mygroup/' + digest, subject.find_path_for_image(image, 'mygroup'))
+
+    def test_find_path_strips_slashes_from_image_group(self):
+        subject = im_manager.DefaultImageManager(self._tempfile)
+        image = np.random.randint(0, 255, dtype=np.uint8, size=(64, 64, 3))
+        digest = xxhash.xxh64(image, seed=0).hexdigest()
+        self.assertEqual('mygroup/mysubgroup/' + digest, subject.find_path_for_image(image, '/mygroup/mysubgroup//'))
+
+    def test_find_path_with_both_groups(self):
+        subject = im_manager.DefaultImageManager(self._tempfile, group_name='/test/foo/')
+        image = np.random.randint(0, 255, dtype=np.uint8, size=(64, 64, 3))
+        digest = xxhash.xxh64(image, seed=0).hexdigest()
+        self.assertEqual('test/foo/mygroup/mysubgroup/' + digest,
+                         subject.find_path_for_image(image, '/mygroup/mysubgroup/'))
