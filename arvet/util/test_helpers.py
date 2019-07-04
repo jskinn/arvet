8ee3ddaedb0757eac3797454a4645f177da3b2a3
John Skinner <jskinn@protonmail.com>
2019-07-04 13:36:05 +1000

    Tweak my test helpers to allow custom messages, and adjusted tolerances for isclose

--- arvet/util/test_helpers.py
+++ arvet/util/test_helpers.py
@@ -7,11 +7,11 @@ class ExtendedTestCase(unittest.TestCase):
     Some utility extensions to the standard unittest test case, for working with numpy arrays.
     """
 
-    def assertNPEqual(self, array_1, array_2):
+    def assertNPEqual(self, array_1, array_2, msg=''):
         self.assertTrue(np.array_equal(array_1, array_2),
-                        "Arrays {0} and {1} are not equal".format(str(array_1), str(array_2)))
+                        "{0}: Arrays {1} and {2} are not equal".format(msg, str(array_1), str(array_2)))
 
-    def assertNPClose(self, array_1, array_2):
-        self.assertTrue(np.all(np.isclose(array_1, array_2)),
-                        "Arrays {0} and {1} are not close".format(str(array_1), str(array_2)))
+    def assertNPClose(self, array_1, array_2, msg='', rtol=1e-05, atol=1e-08):
+        self.assertTrue(np.all(np.isclose(array_1, array_2, rtol=rtol, atol=atol)),
+                        "{0}: Arrays {1} and {2} are not close".format(msg, str(array_1), str(array_2)))
 
