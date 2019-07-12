dfc058837dc3314844f5aaac3ce1db46772cb6e4
John Skinner <jskinn@protonmail.com>
2019-07-13 01:05:40 +1000

    Change run_task to use argparse, and make sure it imports the relevant task subtypes, to ensure that the tasks actually load.

--- arvet/batch_analysis/scripts/run_task.py
+++ arvet/batch_analysis/scripts/run_task.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 # Copyright (c) 2017, John Skinner
-import sys
-import os
 import logging
 import logging.config
 import traceback
+import argparse
 from bson import ObjectId
 
 from arvet.config.global_configuration import load_global_config
@@ -13,63 +12,66 @@ import arvet.database.connection as dbconn
 import arvet.database.image_manager as im_manager
 from arvet.batch_analysis.task import Task
 
-
-def patch_cwd():
-    """
-    Patch sys.path to make sure the current working directory is included.
-    This is necessary when this is being used as a library,
-    and we run the script by file path.
-    :return:
-    """
-    cwd = os.getcwd()
-    if cwd not in sys.path:
-        sys.path.append(cwd)
+# These imports are necessary for the tasks to actually load the task subclasses
+# noinspection PyUnresolvedReferences
+from arvet.batch_analysis.tasks.import_dataset_task import ImportDatasetTask
+# noinspection PyUnresolvedReferences
+from arvet.batch_analysis.tasks.measure_trial_task import MeasureTrialTask
+# noinspection PyUnresolvedReferences
+from arvet.batch_analysis.tasks.run_system_task import RunSystemTask
 
 
-def main(*args):
+def main(task_id: str, config_file: str = 'config.yml'):
     """
     Run a particular task.
     :args: Only argument is the id of the task to run
     :return:
     """
-    if len(args) >= 1:
-        task_id = ObjectId(args[0])
-        # patch_cwd()
+    task_id = ObjectId(task_id)
 
-        # Load the configuration
-        config = load_global_config('config.yml')
-        if __name__ == '__main__':
-            # Only configure the logging if this is the main function, don't reconfigure
-            logging.config.dictConfig(config['logging'])
+    # Load the configuration
+    config = load_global_config(config_file)
+    if __name__ == '__main__':
+        # Only configure the logging if this is the main function, don't reconfigure
+        logging.config.dictConfig(config['logging'])
 
-        # Configure the database and the image manager
-        dbconn.configure(config['database'])
-        im_manager.configure(config['image_manager'])
+    # Configure the database and the image manager
+    dbconn.configure(config['database'])
+    im_manager.configure(config['image_manager'])
 
-        # Set up the path manager
-        path_manger = PathManager(paths=config['paths'])
+    # Set up the path manager
+    path_manger = PathManager(paths=config['paths'])
 
-        # Try and get the task object
-        try:
-            task = Task.objects.get({'_id': task_id})
-        except Exception as ex:
-            logging.getLogger(__name__).critical("Exception occurred while loading Task({0}):\n{1}".format(
-                str(task_id), traceback.format_exc()
-            ))
-            raise ex
+    # Try and get the task object
+    try:
+        task = Task.objects.get({'_id': task_id})
+    except Exception as ex:
+        logging.getLogger(__name__).critical("Exception occurred while loading Task({0}):\n{1}".format(
+            str(task_id), traceback.format_exc()
+        ))
+        raise ex
 
-        # Since we got the task, try and run it
-        try:
-            task.run_task(path_manger)
-        except Exception as ex:
-            logging.getLogger(__name__).critical("Exception occurred while running {0}({1}):\n{2}".format(
-                type(task).__name__, str(task_id), traceback.format_exc()
-            ))
-            task.mark_job_failed()
-            raise ex
-        finally:
-            task.save()
+    # Since we got the task, try and run it
+    try:
+        task.run_task(path_manger)
+    except Exception as ex:
+        logging.getLogger(__name__).critical("Exception occurred while running {0}({1}):\n{2}".format(
+            type(task).__name__, str(task_id), traceback.format_exc()
+        ))
+        task.mark_job_failed()
+        raise ex
+    finally:
+        task.save()
 
 
 if __name__ == '__main__':
-    main(*sys.argv[1:])
+    parser = argparse.ArgumentParser(
+        description='Run a particular task, by id.'
+                    'This should be called automatically by the job system. Common to all task types.')
+    parser.add_argument('--config', default='config.yml',
+                        help='The path to the config file to use. default to \'config.yml\'')
+    parser.add_argument('task_id',
+                        help='The id of the task to run.')
+
+    args = parser.parse_args()
+    main(args.task_id, args.config)
