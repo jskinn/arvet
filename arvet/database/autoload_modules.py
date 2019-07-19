60f483b3dd4ec734b0f7d464d2885bcab6778964
John Skinner <jskinn@protonmail.com>
2019-07-19 23:11:00 +1000

    Add a function to automatically retrieve model classes, without necessarily loading the models themselves. Lets me use class methods and the type heirarchy to store meta information.

--- arvet/database/autoload_modules.py
+++ arvet/database/autoload_modules.py
@@ -1,33 +1,43 @@
 import typing
 import sys
-import bson
 import importlib
 import logging
 from pymodm.base.models import MongoModelMetaclass
 
 
-def autoload_modules(model: MongoModelMetaclass, ids: typing.List[bson.ObjectId] = None):
+def autoload_modules(model: MongoModelMetaclass, ids: typing.List[typing.Any] = None) -> None:
     """
+    Find and automatically load the subtypes of a given model class.
+    Used to allow me to use base model types with many different subclasses,
+    and let the experiments hold custom code.
+    Systems, Metrics, and Experiments are some examples of this pattern.
 
-    :param model:
-    :param ids:
-    :return:
+    :param model: The base model to query
+    :param ids: An optional list of ids, will limit the loaded models to those with the given ids
+    :return: None
     """
-    if model._mongometa.final:
-        # The model is final, and has no subclasses, don't load anything
+    # First, get the pymodm metainformation
+    if hasattr(model, '_mongometa'):
+        mongometa = model._mongometa
+    else:
+        raise RuntimeError("Object is a pymodm model, but doesn't have meta information?")
+
+    # If the model is final, and has no subclasses, don't load anything
+    if mongometa.final:
         return
 
+    # Work out which classes we want to load
     query = {'_cls': {'$exists': True}}
     if ids is not None:
         if len(ids) == 1:
             query['_id'] = ids[0]
         else:
             query['_id'] = {'$in': ids}
+
     # Use the mongo collection directly to find the set of all the classes of models in the collection
-    cursor = model._mongometa.collection.find(query, {'_id': False, '_cls': True})
-    for result in cursor:
+    cursor = mongometa.collection.distinct('_cls', query)
+    for type_name in cursor:
         # For each class we found, try and load the module it contains
-        type_name = result['_cls']
         module_name = type_name.rpartition('.')[0]
         if module_name is not '' and module_name not in sys.modules:
             try:
@@ -37,3 +47,66 @@ def autoload_modules(model: MongoModelMetaclass, ids: typing.List[bson.ObjectId]
                     "Could not import module {0} containing model {1},"
                     "models of that type will not be returned".format(module_name, type_name))
                 pass
+
+
+def get_model_classes(model: MongoModelMetaclass, ids: typing.List[typing.Any]) -> typing.List[MongoModelMetaclass]:
+    """
+    Get the model class objects for specific objects, useful to run class methods
+    Will try and load the models if they are missing.
+    Missing classes or modules are logged, but do not raise errors.
+
+    :param model: The base model type, providing the meta-information like which collection to use
+    :param ids: The list of ids to find the classes of
+    :return: A list of model objects
+    """
+    # First, get the pymodm metainformation
+    if hasattr(model, '_mongometa'):
+        mongometa = model._mongometa
+    else:
+        raise RuntimeError("Object is a pymodm model, but doesn't have meta information?")
+
+    # If the model is final, it has no subclasses, so there is nothing to find.
+    if mongometa.final:
+        # The model is final, and has no subclasses, return the model
+        return [model]
+
+    # Work out which classes we want to load
+    query = {'_cls': {'$exists': True}}
+    if ids is not None:
+        if len(ids) == 1:
+            query['_id'] = ids[0]
+        else:
+            query['_id'] = {'$in': ids}
+
+    # Use the mongo collection directly to find the set of all the classes of models in the collection
+    model_classes = []
+    cursor = mongometa.collection.distinct('_cls', query)
+    for type_name in cursor:
+        # For each class we found, try and load the module it contains
+        module_name, _, class_name = type_name.rpartition('.')
+
+        # Step 1: find the module containing the class
+        if module_name is '':
+            continue
+        else:
+            if module_name in sys.modules:
+                # Module is already loaded, read it
+                module = sys.modules[module_name]
+            else:
+                # Module is not loaded, try and load it
+                try:
+                    module = importlib.import_module(module_name)
+                except ImportError:
+                    logging.getLogger(__name__).warning(
+                        "Could not import module {0} containing model {1},"
+                        "models of that type will not be returned".format(module_name, type_name))
+                    continue
+
+        # Then, if we found a found a module, try and get the model from it
+        if module is not None:
+            if hasattr(module, class_name):
+                model_classes.append(getattr(module, class_name))
+            else:
+                logging.getLogger(__name__).warning(
+                    "Could not find model class {0} in module {1}".format(class_name, module_name))
+    return model_classes
--- arvet/database/tests/test_autoload_modules.py
+++ arvet/database/tests/test_autoload_modules.py
@@ -1,9 +1,11 @@
 import unittest
 import unittest.mock as mock
+import sys
+import importlib
 import pymodm
 import arvet.database.tests.database_connection as dbconn
 from .mock_base_model import TestBaseModel
-from arvet.database.autoload_modules import autoload_modules
+from arvet.database.autoload_modules import autoload_modules, get_model_classes
 
 
 class TestSubclassModel(TestBaseModel):
@@ -22,19 +24,27 @@ class TestFinalModel(pymodm.MongoModel):
 
 
 class TestAutoloadModulesDatabase(unittest.TestCase):
+    _orig_sys_modules = None
+    _orig_import_module = None
 
     @classmethod
     def setUpClass(cls):
         dbconn.connect_to_test_db()
+        cls._orig_sys_modules = sys.modules
+        cls._orig_import_module = importlib.import_module
 
     @classmethod
     def tearDownClass(cls):
         # Clean up after ourselves by dropping the collection for this model and removing the image file
         TestBaseModel._mongometa.collection.drop()
 
-    @mock.patch('arvet.database.autoload_modules.importlib.sys.modules')
-    @mock.patch('arvet.database.autoload_modules.importlib.import_module')
-    def test_tries_to_load_all_the_modules_that_exist(self, mock_import_modules, mock_sys_modules):
+        # check we haven't contaminated global state
+        assert sys.modules == cls._orig_sys_modules
+        assert importlib.import_module == cls._orig_import_module
+
+    @mock.patch('arvet.database.autoload_modules.sys', spec=['modues'])
+    @mock.patch('arvet.database.autoload_modules.importlib.import_module', autospec=True)
+    def test_tries_to_load_all_the_modules_that_exist(self, mock_import_module, mock_sys):
         module_names = []
 
         model = TestBaseModel()
@@ -46,17 +56,17 @@ class TestAutoloadModulesDatabase(unittest.TestCase):
         module_names.append(model.__module__)
 
         # Make it seem like no modules are loaded
-        mock_sys_modules.__contains__.return_value = False
+        mock_sys.modules = {}
 
         autoload_modules(TestBaseModel)
 
-        self.assertTrue(mock_import_modules.called)
+        self.assertTrue(mock_import_module.called)
         for module_name in module_names:
-            self.assertIn(mock.call(module_name), mock_import_modules.call_args_list)
+            self.assertIn(mock.call(module_name), mock_import_module.call_args_list)
 
-    @mock.patch('arvet.database.autoload_modules.importlib.sys.modules')
-    @mock.patch('arvet.database.autoload_modules.importlib.import_module')
-    def test_only_looks_for_models_for_the_given_ids(self, mock_import_modules, mock_sys_modules):
+    @mock.patch('arvet.database.autoload_modules.sys', spec=['modules'])
+    @mock.patch('arvet.database.autoload_modules.importlib.import_module', autospec=True)
+    def test_only_looks_for_models_for_the_given_ids(self, mock_import_module, mock_sys):
         base_model = TestBaseModel()
         base_model.save()
 
@@ -64,33 +74,33 @@ class TestAutoloadModulesDatabase(unittest.TestCase):
         sub_model.save()
 
         # Make it seem like no modules are loaded
-        mock_sys_modules.__contains__.return_value = False
+        mock_sys.modules = {}
 
         autoload_modules(TestBaseModel, ids=[sub_model.pk])
 
-        self.assertTrue(mock_import_modules.called)
-        self.assertEqual(1, len(mock_import_modules.call_args_list))
-        self.assertNotIn(mock.call(base_model.__module__), mock_import_modules.call_args_list)
-        self.assertIn(mock.call(sub_model.__module__), mock_import_modules.call_args_list)
+        self.assertTrue(mock_import_module.called)
+        self.assertEqual(1, len(mock_import_module.call_args_list))
+        self.assertNotIn(mock.call(base_model.__module__), mock_import_module.call_args_list)
+        self.assertIn(mock.call(sub_model.__module__), mock_import_module.call_args_list)
 
-    @mock.patch('arvet.database.autoload_modules.importlib.sys.modules')
-    @mock.patch('arvet.database.autoload_modules.importlib.import_module')
-    def test_doesnt_load_any_modules_when_given_no_ids(self, mock_import_modules, mock_sys_modules):
+    @mock.patch('arvet.database.autoload_modules.sys', spec=['modules'])
+    @mock.patch('arvet.database.autoload_modules.importlib.import_module', autospec=True)
+    def test_doesnt_load_any_modules_when_given_no_ids(self, mock_import_module, mock_sys):
         model = TestBaseModel()
         model.save()
         model = TestSubclassModel()
         model.save()
 
         # Make it seem like no modules are loaded
-        mock_sys_modules.__contains__.return_value = False
+        mock_sys.modules = {}
 
         autoload_modules(TestBaseModel, ids=[])
 
-        self.assertFalse(mock_import_modules.called)
+        self.assertFalse(mock_import_module.called)
 
-    @mock.patch('arvet.database.autoload_modules.importlib.sys.modules')
-    @mock.patch('arvet.database.autoload_modules.importlib.import_module')
-    def test_doesnt_reload_modules_already_loaded(self, mock_import_modules, mock_sys_modules):
+    @mock.patch('arvet.database.autoload_modules.sys', spec=['modules'])
+    @mock.patch('arvet.database.autoload_modules.importlib.import_module', autospec=True)
+    def test_doesnt_reload_modules_already_loaded(self, mock_import_module, mock_sys):
         base_model = TestBaseModel()
         base_model.save()
 
@@ -98,23 +108,243 @@ class TestAutoloadModulesDatabase(unittest.TestCase):
         sub_model.save()
 
         # Make it seem like only one module is loaded
-        mock_sys_modules.__contains__.side_effect = lambda m: m == base_model.__module__
+        mock_sys.modules = {base_model.__module__: mock.Mock()}
 
         autoload_modules(TestBaseModel)
 
-        self.assertTrue(mock_import_modules.called)
-        self.assertEqual(1, len(mock_import_modules.call_args_list))
-        self.assertNotIn(mock.call(base_model.__module__), mock_import_modules.call_args_list)
-        self.assertIn(mock.call(sub_model.__module__), mock_import_modules.call_args_list)
+        self.assertTrue(mock_import_module.called)
+        self.assertEqual(1, len(mock_import_module.call_args_list))
+        self.assertNotIn(mock.call(base_model.__module__), mock_import_module.call_args_list)
+        self.assertIn(mock.call(sub_model.__module__), mock_import_module.call_args_list)
 
-    @mock.patch('arvet.database.autoload_modules.importlib.sys.modules')
-    @mock.patch('arvet.database.autoload_modules.importlib.import_module')
-    def test_doesnt_load_modules_for_final_models(self, mock_import_modules, mock_sys_modules):
+    @mock.patch('arvet.database.autoload_modules.sys', spec='modules')
+    @mock.patch('arvet.database.autoload_modules.importlib.import_module', autospec=True)
+    def test_doesnt_load_modules_for_final_models(self, mock_import_module, mock_sys):
         model = TestFinalModel()
         model.save()
 
         # Make it seem like no modules are loaded
-        mock_sys_modules.__contains__.return_value = False
+        mock_sys.modules = {}
 
         autoload_modules(TestFinalModel)
-        self.assertFalse(mock_import_modules.called)
+        self.assertFalse(mock_import_module.called)
+
+
+class TestGetModelClassesDatabase(unittest.TestCase):
+    _orig_sys_modules = None
+    _orig_import_module = None
+
+    @classmethod
+    def setUpClass(cls):
+        dbconn.connect_to_test_db()
+        cls._orig_sys_modules = sys.modules
+        cls._orig_import_module = importlib.import_module
+
+    @classmethod
+    def tearDownClass(cls):
+        # Clean up after ourselves by dropping the collection for this model and removing the image file
+        TestBaseModel._mongometa.collection.drop()
+
+        # check we haven't contaminated global state
+        assert sys.modules == cls._orig_sys_modules
+        assert importlib.import_module == cls._orig_import_module
+
+    def test_returns_model_classes_for_ids(self):
+        model_ids = []
+
+        model = TestBaseModel()
+        model.save()
+        model_ids.append(model.pk)
+
+        model = TestSubclassModel()
+        model.save()
+        model_ids.append(model.pk)
+
+        result = get_model_classes(TestBaseModel, model_ids)
+        self.assertEqual(2, len(result))
+        self.assertIn(TestBaseModel, result)
+        self.assertIn(TestSubclassModel, result)
+
+    def test_returns_models_for_only_requested_ids(self):
+        model_ids = []
+
+        # put this model in the database, but we're not going to request it
+        model = TestBaseModel()
+        model.save()
+
+        model = TestSubclassModel()
+        model.save()
+        model_ids.append(model.pk)
+
+        result = get_model_classes(TestBaseModel, model_ids)
+        self.assertEqual(1, len(result))
+        self.assertNotIn(TestBaseModel, result)
+        self.assertIn(TestSubclassModel, result)
+
+    def test_returns_unique_models(self):
+        model_ids = []
+
+        model = TestBaseModel()
+        model.save()
+
+        # Make many objects of the same type, we're going to request them all
+        for _ in range(3):
+            model = TestSubclassModel()
+            model.save()
+            model_ids.append(model.pk)
+
+        result = get_model_classes(TestBaseModel, model_ids)
+        self.assertEqual(1, len(result))
+        self.assertNotIn(TestBaseModel, result)
+        self.assertIn(TestSubclassModel, result)
+
+    @mock.patch('arvet.database.autoload_modules.sys', spec=['modules'])
+    @mock.patch('arvet.database.autoload_modules.importlib.import_module', autospec=True)
+    def test_returns_but_doesnt_try_and_load_subclasses_of_final_models(self, mock_import_module, mock_sys):
+        model_ids = []
+        module_names = []
+
+        model = TestFinalModel()
+        model.save()
+        model_ids.append(model.pk)
+        module_names.append(model.__module__)
+
+        # Make it seem like no modules are loaded, but that we can load modules
+        mock_sys.modules = {}
+        mock_import_module.return_value = make_mock_module(TestFinalModel)
+
+        # Mock the collection, to make sure we're not calling anything on it
+        with mock.patch.object(TestFinalModel, '_mongometa') as mock_meta:
+            mock_meta.final = True
+            mock_collection = mock.Mock()
+            mock_meta.collection = mock_collection
+
+            result = get_model_classes(TestFinalModel, model_ids)
+            self.assertEqual(1, len(result))
+            self.assertIn(TestFinalModel, result)
+            self.assertFalse(mock_import_module.called)
+            self.assertEqual([], mock_collection.method_calls)
+
+    @mock.patch('arvet.database.autoload_modules.sys', spec=['modules'])
+    @mock.patch('arvet.database.autoload_modules.importlib.import_module', autospec=True)
+    def test_tries_to_load_models_that_dont_exist(self, mock_import_module, mock_sys):
+        model_ids = []
+        module_names = []
+
+        model = TestBaseModel()
+        model.save()
+        model_ids.append(model.pk)
+        module_names.append(model.__module__)
+
+        model = TestSubclassModel()
+        model.save()
+        model_ids.append(model.pk)
+        module_names.append(model.__module__)
+
+        # Make it seem like no modules are loaded, but that we can load modules
+        mock_sys.modules = {}
+        mock_import_module.return_value = make_mock_module(TestBaseModel, TestSubclassModel)
+
+        result = get_model_classes(TestBaseModel, model_ids)
+        self.assertEqual(2, len(result))
+        self.assertIn(TestBaseModel, result)
+        self.assertIn(TestSubclassModel, result)
+
+        self.assertTrue(mock_import_module.called)
+        for module_name in module_names:
+            self.assertIn(mock.call(module_name), mock_import_module.call_args_list)
+
+    @mock.patch('arvet.database.autoload_modules.logging', spec=['getLogger'])
+    @mock.patch('arvet.database.autoload_modules.sys', spec=['modules'])
+    @mock.patch('arvet.database.autoload_modules.importlib.import_module', autospec=True)
+    def test_fails_with_log_on_unimportable_modules(self, mock_import_module, mock_sys, mock_logging):
+        model_ids = []
+        module_names = []
+
+        model = TestBaseModel()
+        model.save()
+        model_ids.append(model.pk)
+        module_names.append(model.__module__)
+
+        model = TestSubclassModel()
+        model.save()
+        model_ids.append(model.pk)
+        module_names.append(model.__module__)
+
+        # Make it seem like no modules are loaded, but that we can load modules
+        mock_logger = make_mock_logger(mock_logging)
+        mock_sys.modules = {}
+        mock_module = make_mock_module(TestBaseModel, TestSubclassModel)
+
+        def occasional_error(name):
+            if name == TestBaseModel.__module__:
+                return mock_module
+            raise ImportError("Test error")
+        mock_import_module.side_effect = occasional_error
+
+        # Check we returned the class that we can import successfully
+        result = get_model_classes(TestBaseModel, model_ids)
+        self.assertEqual(1, len(result))
+        self.assertIn(TestBaseModel, result)
+        self.assertNotIn(TestSubclassModel, result)
+
+        # Check we still tried to import all the modules
+        self.assertTrue(mock_import_module.called)
+        for module_name in module_names:
+            self.assertIn(mock.call(module_name), mock_import_module.call_args_list)
+
+        # Check that the fact that we couldn't load a module was logged
+        self.assertTrue(mock_logger.warning.called)
+        log_msg = mock_logger.warning.call_args[0][0]
+        self.assertIn(TestSubclassModel.__module__, log_msg)
+
+    @mock.patch('arvet.database.autoload_modules.logging', spec=['getLogger'])
+    @mock.patch('arvet.database.autoload_modules.sys', spec=['modules'])
+    @mock.patch('arvet.database.autoload_modules.importlib.import_module', autospec=True)
+    def test_fails_with_log_on_missing_classes(self, mock_import_module, mock_sys, mock_logging):
+        model_ids = []
+        module_names = []
+
+        model = TestBaseModel()
+        model.save()
+        model_ids.append(model.pk)
+        module_names.append(model.__module__)
+
+        model = TestSubclassModel()
+        model.save()
+        model_ids.append(model.pk)
+        module_names.append(model.__module__)
+
+        # Make it seem like no modules are loaded, but that we can load modules
+        mock_logger = make_mock_logger(mock_logging)
+        mock_sys.modules = {}
+        mock_import_module.return_value = make_mock_module(TestBaseModel)
+
+        # Check we returned the class that we can import successfully
+        result = get_model_classes(TestBaseModel, model_ids)
+        self.assertEqual(1, len(result))
+        self.assertIn(TestBaseModel, result)
+        self.assertNotIn(TestSubclassModel, result)
+
+        # Check we still tried to import all the modules
+        self.assertTrue(mock_import_module.called)
+        for module_name in module_names:
+            self.assertIn(mock.call(module_name), mock_import_module.call_args_list)
+
+        # Check that the fact that we couldn't load a module was logged
+        self.assertTrue(mock_logger.warning.called)
+        log_msg = mock_logger.warning.call_args[0][0]
+        self.assertIn(TestSubclassModel.__name__, log_msg)
+
+
+def make_mock_module(*module_classes):
+    mock_module = mock.Mock(spec=[module_class.__name__ for module_class in module_classes])
+    for module_class in module_classes:
+        setattr(mock_module, module_class.__name__, module_class)
+    return mock_module
+
+
+def make_mock_logger(mock_logging):
+    mock_logger = mock.Mock(spec=['warning', 'error'])
+    mock_logging.getLogger.return_value = mock_logger
+    return mock_logger
